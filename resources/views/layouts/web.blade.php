4455314d91035e11cbb6d1ba0a9c79a9db32b778
dinukolla <dinukolla@.github.com>
2020-11-18 13:02:30 +0530

    line awesome

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -9,6 +9,9 @@
    
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    
+    <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+    {{-- <link rel="stylesheet" href="{{ asset('css/line-awesome.css') }}"> --}}
 </head>
 <body>
 
--- resources/views/website/home.blade.php
+++ resources/views/website/home.blade.php
@@ -2,7 +2,8 @@
 @section('content')
 <!-- As a link -->
 <nav class="navbar navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="#">Navbar<i class="lar la-bell"></i></a>
+  
 </nav>
 <input type="button" class="btn btn-success" value="casbhkcv">
 @endsection
