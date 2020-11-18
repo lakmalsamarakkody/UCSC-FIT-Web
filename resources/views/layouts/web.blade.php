7c87a719c85559daa2a60765dd000f525b64c189
lakmalsamarakkody <43323601+lakmalsamarakkody@users.noreply.github.com>
2020-11-18 13:58:33 +0530

    navigation

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -1,21 +1,53 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-   
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>FIT - UCSC</title>
+
+        <!-- SCRIPTS -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+        <!-- /SCRIPTS -->
     
-    <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
-    {{-- <link rel="stylesheet" href="{{ asset('css/line-awesome.css') }}"> --}}
-</head>
-<body>
+        <!-- STYLES -->
+            <!-- BOOTSTRAP -->      <link rel="stylesheet" href="{{ asset('css/app.css') }}" >
+            <!-- LINE AWESOME -->   <link rel="stylesheet" href="{{ asset('css/line-awesome/css/line-awesome.css') }}">
+        <!-- /STYLES -->
 
-    @yield('content')
+    </head>
 
-</body>
+    <body>
+
+        <!-- NAVIGATION -->
+        <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 text-right text-white bg-dark">Login</div>
+            <div class="col-12 text-center"><h4>Foundation In Information Technology</h4></div>
+
+            <div class="container-fluid">
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <a class="navbar-brand" href="home">Home</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div class="navbar-nav ml-auto">
+                            <!-- <a class="nav-link active" href="#">Home <span class="sr-only">(current)</span></a> -->
+                            <a class="nav-link" href="test">Programme</a>
+                            <a class="nav-link" href="#">Registration</a>
+                            <a class="nav-link" href="#">Learning</a>
+                            <a class="nav-link" href="#">Examination</a>
+                            <a class="nav-link" href="#">Contact Us</a>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+        </div>
+        <!-- /NAVIGATION -->
+
+        @yield('content')
+
+    </body>
 </html>
