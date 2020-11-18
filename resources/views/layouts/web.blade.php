f36a690e73e5e66b78b07de768a240c48138be6d
dinukolla <dinukolla@.github.com>
2020-11-18 17:39:22 +0530

    Update web.blade.php

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -68,13 +68,7 @@
         &copy; Copyright <strong>UCSC</strong>. All Rights Reserved
       </div>
       <div class="credits">
-        <!--
-          All the links in the footer should remain intact.
-          You can delete the links only if you purchased the pro version.
-          Licensing information: https://bootstrapmade.com/license/
-          Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=Regna
-        -->
-        Designed by <a href="https://www.linkedin.com/in/dinusha-kulasooriya-599a68a6/a">e-Learning Center- UCSC</a>
+        Designed by <a href="#">e-Learning Center- UCSC</a>
       </div>
     </div>
   </footer><!-- #footer -->
