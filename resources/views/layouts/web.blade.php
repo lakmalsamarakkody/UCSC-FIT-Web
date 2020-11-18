83ab8e867a85a991e4f29489fe70c159d0473046
dinukolla <dinukolla@.github.com>
2020-11-18 17:33:53 +0530

    dinusha

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -6,48 +6,100 @@
 
         <title>FIT - UCSC</title>
 
+        <!-- Favicon -->
+        <link rel="icon" type="image/png" href="/img/favicon.png">
+
+        <!-- FONTS -->
+            <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Poppins:300,400,500,700" rel="stylesheet">
+        <!-- /FONTS -->
+
         <!-- SCRIPTS -->
         <script src="{{ asset('js/app.js') }}" defer></script>
         <!-- /SCRIPTS -->
     
         <!-- STYLES -->
             <!-- BOOTSTRAP -->      <link rel="stylesheet" href="{{ asset('css/app.css') }}" >
-            <!-- LINE AWESOME -->   <link rel="stylesheet" href="{{ asset('css/line-awesome/css/line-awesome.css') }}">
+            <!-- CSS -->      <link rel="stylesheet" href="{{ asset('css/web.css') }}" >
+            <!-- LINE AWESOME -->   <link rel="stylesheet" href="{{ asset('lib/font-awesome/css/font-awesome.min.css') }}">
+            <!-- ANIMATE -->        <link href="{{ asset('lib/animate/animate.min.css') }}" rel="stylesheet">
         <!-- /STYLES -->
 
-    </head>
 
-    <body>
 
-        
-        <div class="container-fluid">
-        <div class="row">
-            <div class="col-12 text-right text-white bg-dark">Login</div>
-            
-            <div class="container">
-                <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                    <a class="navbar-brand" href="home"><img src="{{ asset('img/fit.png') }}" width="70" height="45"/></a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                        <div class="navbar-nav ml-auto">
-                            <!-- <a class="nav-link active" href="#">Home <span class="sr-only">(current)</span></a> -->
-                            <a class="nav-link" href="#">Programme</a>
-                            <a class="nav-link" href="#">Registration</a>
-                            <a class="nav-link" href="#">Learning</a>
-                            <a class="nav-link" href="#">Examination</a>
-                            <a class="nav-link" href="#">Contact Us</a>
-                        </div>
-                    </div>
-                </nav>
-            </div>
+<body>
+  <!--==========================
+  Header
+  ============================-->
+  <header id="header">
 
-        </div>
-        </div>
-        <!-- /NAVIGATION -->
+      <div id="logo" class="pull-left">
+        <a href="#hero"><img src="img/fit.png" alt="" title="" style="width: 60px; padding: 0px 0px 0px 0px;"/></img></a>
+
+      </div>
+
+      <nav id="nav-menu-container">
+        <ul class="nav-menu">
+          <li class="menu-active"><a href="#hero">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#">Programme</a></li>
+          <li><a href="#">Registration</a></li>
+          <li><a href="#">Learning</a></li>
+          <li><a href="#">Examination</a></li>
+          <li><a href="#">Contact Us</a></li>
+
+        </ul>
+      </nav><!-- #nav-menu-container -->
+    </div>
+  </header><!-- #header -->
 
         @yield('content')
 
-    </body>
+  <!--==========================
+    Footer
+  ============================-->
+  <footer id="footer">
+    <div class="footer-top">
+      <div class="container">
+
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong>UCSC</strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        <!--
+          All the links in the footer should remain intact.
+          You can delete the links only if you purchased the pro version.
+          Licensing information: https://bootstrapmade.com/license/
+          Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=Regna
+        -->
+        Designed by <a href="https://www.linkedin.com/in/dinusha-kulasooriya-599a68a6/a">e-Learning Center- UCSC</a>
+      </div>
+    </div>
+  </footer><!-- #footer -->
+
+  <a href="#" class="back-to-top"><i class="la la-chevron-up"></i></a>
+
+  <!-- JavaScript Libraries -->
+  <script src="{{ asset('lib/jquery/jquery.min.js') }}"></script>
+  <script src="{{ asset('lib/jquery/jquery-migrate.min.js') }}"></script>
+  <script src="{{ asset('lib/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ asset('lib/easing/easing.min.js') }}"></script>
+  <script src="{{ asset('lib/wow/wow.min.js') }}"></script>
+  <script src="{{ asset('lib/waypoints/waypoints.min.js') }}"></script>
+  <script src="{{ asset('lib/counterup/counterup.min.js') }}"></script>
+  <script src="{{ asset('lib/superfish/hoverIntent.js') }}"></script>
+  <script src="{{ asset('lib/superfish/superfish.min.js') }}"></script>
+
+
+  <!-- Template Main Javascript File -->
+  <script src="{{ asset('js/web.js') }}"></script>
+
+</body>
+
+
+
+
 </html>
--- resources/views/website/program.blade.php
+++ resources/views/website/program.blade.php
@@ -1,4 +1,58 @@
 @extends('layouts/web')
 @section('content')
-programme structure
+
+<!--==========================
+Hero Section
+============================-->
+<section id="hero" style="background-image: url({{ asset('img/hero-back.png') }}); height:200px">
+<div class="page-hero-container">
+    <h1 style="padding-top:30px">Programme</h1>
+</div>
+</section><!-- #hero -->
+
+  <main id="main">
+
+    <!--==========================
+      About Us Section
+    ============================-->
+    <section id="about">
+      <div class="container">
+        <div class="row about-container">
+
+          <div class="col-lg-8 content order-lg-1 order-2">
+            <h2 class="title">About FIT</h2>
+            <p>
+              The Foundation in Information Technology (FIT) programme aims at enhancing the literacy and competency in using basic computer applications together with analytical thinking and communicational skills required for school leavers. Hence, Mathematics and English Language for Information Communication Technology (ICT) are considered as two other important supporting subject domains.
+
+                FIT is a pre-degree programme that prepares students who are willing to read for their first degree. It is designed for anyone irrespective of the study streams they have followed in Advanced Level (secondary education. Recent statistics show many students who register for degree programmes fail due to lack of ICT competency, language skills and mathematical background. This tendency is very high in external degree programmes. Hence, FIT was designed to address these issues of undergraduate studies.
+
+                At the same time, FIT is an alternative qualification for students who do not posse A/L qualifications to enroll into the Bachelor of Information Technology (BIT) programme. However, those who register for BIT should have completed studies up to Ordinary Level in school curriculum.
+
+                FIT is also a certification programme for employment seekers or school leavers to justify their knowledge and skills in ICT for their future endeavors. University of Colombo School of Computing (UCSC), which is the most reputed higher education institute in the field of computer science and Information Communication Technology in Sri Lanka, will issue these certificates once you successfully complete relevant assessment of the FIT programme.
+
+                The FIT, Foundation in Information Technology, programme consists of three courses, namely, ICT Applications (FIT 103), English for ICT (FIT 203) and Mathematics for ICT (FIT 303). e-Learning based online system will be available for all registered students to follow courses at any time during the day. e-Testing based system will be used for the evaluation once the student is ready to take the test at University of Colombo School of Computing (UCSC) etesting lab. Hence, students could start the programme at any time during the year and they can take exams at any month during the year at testing centres. At the same time, several private institutes conduct face to face classes based on the FIT syllabuses.
+                </p>
+            <div class="icon-box wow fadeInUp" data-wow-delay="0.2s">
+              <div class="icon"><i class="fa fa-globe"></i></div>
+              <h4 class="title"><a href="">Programme</a></h4>
+              <p class="description">For Programme Structure</p>
+            </div>
+
+          </div>
+
+          <div class="col-lg-4 background order-lg-2 order-1 wow fadeInRight" style="background-image: url({{ asset('img/fit.png') }}); margin-top: 100px"></div>
+        </div>
+
+      </div>
+    </section><!-- #about -->
+
+
+
+  </main>
+
+
+
+
+
+
 @endsection
