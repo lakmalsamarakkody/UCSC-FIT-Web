12fb678c2be3e350d822233bf3eb824c5037238d
dinukolla <dinukolla@.github.com>
2020-11-18 16:38:12 +0530

    dinusha

--- resources/views/website/home.blade.php
+++ resources/views/website/home.blade.php
@@ -27,7 +27,9 @@
 
 
 <body>
-
+<button type="button" id="mobile-nav-toggle">
+  <i class="fa fa-bars"></i>
+</button>
   <!--==========================
   Header
   ============================-->
@@ -35,21 +37,19 @@
 
       <div id="logo" class="pull-left">
         <a href="#hero"><img src="img/fit.png" alt="" title="" style="width: 60px; padding: 0px 0px 0px 0px;"/></img></a>
-        <!-- Uncomment below if you prefer to use a text logo -->
-        <!--<h1><a href="#hero">Regna</a></h1>-->
+
       </div>
 
       <nav id="nav-menu-container">
         <ul class="nav-menu">
           <li class="menu-active"><a href="#hero">Home</a></li>
           <li><a href="#about">About</a></li>
-          <li><a href="#about">Programme</a></li>
-          <li><a href="#services">Registration</a></li>
-          <li><a href="#services">Learning</a></li>
-          <li><a href="#services">Examination</a></li>
-          <li><a href="#services">Contact Us</a></li>
+          <li><a href="#">Programme</a></li>
+          <li><a href="#">Registration</a></li>
+          <li><a href="#">Learning</a></li>
+          <li><a href="#">Examination</a></li>
+          <li><a href="#">Contact Us</a></li>
 
-          <li><a href="../ncwms-back/index.php" target="_blank">Login</a></li>
         </ul>
       </nav><!-- #nav-menu-container -->
     </div>
@@ -60,8 +60,9 @@
   ============================-->
   <section id="hero" style="background-image: url({{ asset('img/hero-back.png') }});">
     <div class="hero-container">
-      <h1 style="text-align: left;">Foundation in Information Technology</h1>
-      <h2>University of Colombo School of Computing</h2>
+    <img src="{{ asset('img/invert-ucsc.png') }}" width=10% >
+      <h1 style="text-align: left;">Foundation in <br>Information Technology</h1>
+      <h3 style="color:#fff">University of Colombo School of Computing</h2>
       <a href="#about" class="btn-get-started" style="width=25px">About FIT</a>
     </div>
   </section><!-- #hero -->
@@ -75,22 +76,28 @@
       <div class="container">
         <div class="row about-container">
 
-          <div class="col-lg-6 content order-lg-1 order-2">
-            <h2 class="title">Few Words About Us</h2>
+          <div class="col-lg-8 content order-lg-1 order-2">
+            <h2 class="title">About FIT</h2>
             <p>
-              We are an organization which is passionate and dedicated in delivering high quality education and high quality software solutions to meet the industry requirements!
-                                <br>
-                                ISeeQ Pvt Ltd is an organization with a motive of serving the society, established in 2012. As the initial step it started catering education service then diversified its business to providing software solutions to various industries. These business activities become the main funding source for the ultimate motive of ISeeQ that is establishing a proper system to cater needy children and turning them to a productive and healthy citizen for the nation. 
-            </p>
+              The Foundation in Information Technology (FIT) programme aims at enhancing the literacy and competency in using basic computer applications together with analytical thinking and communicational skills required for school leavers. Hence, Mathematics and English Language for Information Communication Technology (ICT) are considered as two other important supporting subject domains.
+
+FIT is a pre-degree programme that prepares students who are willing to read for their first degree. It is designed for anyone irrespective of the study streams they have followed in Advanced Level (secondary education. Recent statistics show many students who register for degree programmes fail due to lack of ICT competency, language skills and mathematical background. This tendency is very high in external degree programmes. Hence, FIT was designed to address these issues of undergraduate studies.
+
+At the same time, FIT is an alternative qualification for students who do not posse A/L qualifications to enroll into the Bachelor of Information Technology (BIT) programme. However, those who register for BIT should have completed studies up to Ordinary Level in school curriculum.
+
+FIT is also a certification programme for employment seekers or school leavers to justify their knowledge and skills in ICT for their future endeavors. University of Colombo School of Computing (UCSC), which is the most reputed higher education institute in the field of computer science and Information Communication Technology in Sri Lanka, will issue these certificates once you successfully complete relevant assessment of the FIT programme.
+
+The FIT, Foundation in Information Technology, programme consists of three courses, namely, ICT Applications (FIT 103), English for ICT (FIT 203) and Mathematics for ICT (FIT 303). e-Learning based online system will be available for all registered students to follow courses at any time during the day. e-Testing based system will be used for the evaluation once the student is ready to take the test at University of Colombo School of Computing (UCSC) etesting lab. Hence, students could start the programme at any time during the year and they can take exams at any month during the year at testing centres. At the same time, several private institutes conduct face to face classes based on the FIT syllabuses.
+</p>
             <div class="icon-box wow fadeInUp" data-wow-delay="0.2s">
-              <div class="icon"><i class="fa fa-globe"></i></div>
-              <h4 class="title"><a href="">For other information</a></h4>
-              <p class="description">Please visit our education provider website from here</p>
+              <div class="icon"><i class="la la-globe"></i></div>
+              <h4 class="title"><a href="">Programme</a></h4>
+              <p class="description">For Programme Structure</p>
             </div>
 
           </div>
 
-          <div class="col-lg-6 background order-lg-2 order-1 wow fadeInRight"></div>
+          <div class="col-lg-4 background order-lg-2 order-1 wow fadeInRight" style="background-image: url({{ asset('img/fit.png') }}); margin-top: 100px"></div>
         </div>
 
       </div>
@@ -126,7 +133,7 @@
     </div>
   </footer><!-- #footer -->
 
-  <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+  <a href="#" class="back-to-top"><i class="la la-chevron-up"></i></a>
 
   <!-- JavaScript Libraries -->
   <script src="{{ asset('lib/jquery/jquery.min.js') }}"></script>
@@ -139,11 +146,9 @@
   <script src="{{ asset('lib/superfish/hoverIntent.js') }}"></script>
   <script src="{{ asset('lib/superfish/superfish.min.js') }}"></script>
 
-  <!-- Contact Form JavaScript File -->
-  <script src="{{ asset('contactform/contactform.j') }}s"></script>
 
   <!-- Template Main Javascript File -->
-  <script src="{{ asset('js/main.js') }}"></script>
+  <script src="{{ asset('js/home.js') }}"></script>
 
 </body>
 
