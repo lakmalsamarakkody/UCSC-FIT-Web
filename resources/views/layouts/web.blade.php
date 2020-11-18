a31b0a82d806fd7dcc0cde327fcd66bce99b6a99
lakmalsamarakkody <43323601+lakmalsamarakkody@users.noreply.github.com>
2020-11-18 14:22:53 +0530

    fit image uploaded

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -19,15 +19,14 @@
 
     <body>
 
-        <!-- NAVIGATION -->
+        
         <div class="container-fluid">
         <div class="row">
             <div class="col-12 text-right text-white bg-dark">Login</div>
-            <div class="col-12 text-center"><h4>Foundation In Information Technology</h4></div>
-
+            
             <div class="container-fluid">
                 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                    <a class="navbar-brand" href="home">Home</a>
+                    <a class="navbar-brand" href="home"><img src="{{ asset('images/fit.jpg') }}" width="279" height="181"/></a>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
@@ -43,6 +42,7 @@
                     </div>
                 </nav>
             </div>
+
         </div>
         </div>
         <!-- /NAVIGATION -->
--- resources/views/website/home.blade.php
+++ resources/views/website/home.blade.php
@@ -1,9 +1,4 @@
 @extends('layouts/web')
 @section('content')
-<!-- As a link -->
-<nav class="navbar navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar<i class="lar la-bell"></i></a>
-  
-</nav>
-<input type="button" class="btn btn-success" value="casbhkcv">
+
 @endsection
