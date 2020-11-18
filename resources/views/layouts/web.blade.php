ef64b961cfdf2a6aa17c326fcbb8b4ebd9c38ecf
lakmalsamarakkody <43323601+lakmalsamarakkody@users.noreply.github.com>
2020-11-18 15:35:52 +0530

    program page created

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -24,9 +24,9 @@
         <div class="row">
             <div class="col-12 text-right text-white bg-dark">Login</div>
             
-            <div class="container-fluid">
+            <div class="container">
                 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                    <a class="navbar-brand" href="home"><img src="{{ asset('images/fit.jpg') }}" width="70" height="45"/></a>
+                    <a class="navbar-brand" href="home"><img src="{{ asset('img/fit.png') }}" width="70" height="45"/></a>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
