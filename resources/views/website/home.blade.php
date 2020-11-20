4ad734bae27781d8f6ce2f7dee7755a1e21b223e
lakmalsamarakkody <43323601+lakmalsamarakkody@users.noreply.github.com>
2020-11-20 19:02:33 +0530

    home page faq completed

--- resources/views/website/home.blade.php
+++ resources/views/website/home.blade.php
@@ -54,88 +54,120 @@
             </div>
         </section><!-- #about -->
 
-        <!-- Accordion -->
+        {{-- FAQ --}}
         <section id="acordion">
+            
             <div class="row about-container">
                 <div class="col-lg-8 content order-lg-1 order-2">
                     <h2 class="title">Frequently Asked Questions</h2>
                 </div>
             </div>
-            <div class=""> 
 
+            <div id="accordion">
 
-                    <h2 class="acc_trigger title"><a href="#toggle1"><i class="fa fa-question-circle pr-3"></i>Why should I follow FIT?</a></h2>
-                    <div class="acc_container">
-                        <div class="block">
-                            <p class="">
-                                <ul>
-                                    <li>It is a pre-degree program for those who wish to follow the <b>Bachelor of Information Technology (BIT)</b>  at UCSC. Moreover, it will be an added qualification for those who seek higher education in ICT or employment in the IT industry or BPO industry.</li>
-                                </ul>
-                            </p>
-                        </div>
-                    </div>
-                    
-                    <h2 class="acc_trigger"><a href="#toggle2"><i class="fa fa-question-circle pr-3"></i>What are the certificates that I can collect by following FIT?</a></h2>
-                    <div class="acc_container">
-                        <div class="block">
-                            <p class="">
-                                <ul>
-                                    <li>There are two certificates. When you pass all three e-Tests of FIT courses, you can get a certificate certifying your scores.</li>
-                                    <li>When you complete all three e-Tests and two practical tests of FIT courses, you can get the final FIT certificate.</li>
-                                </ul>
-                            </p>
-                        </div>
-                    </div>
-                    
-                    <h2 class="acc_trigger"><a href="#toggle2"><i class="fa fa-question-circle pr-3"></i>Why should I follow FIT before I register BIT?</a></h2>
-                    <div class="acc_container">
-                        <div class="block">
-                            <p class="">
-                                <ul>
-                                    <li>You can obtain ICT related certification to find an employment in ICT or related areas as a beginner. (Since BIT is an external degree programme, you could do a full time or part time job while following the BIT Programme <a href="http://www.bit.lk">http://www.bit.lk</a>  ) course</li>
-                                    <li>If you do not have enough qualifications to register BIT programme, you can use FIT certification to obtain BIT registration.</li>
-                                </ul>
-                            </p>
-                        </div>
-                    </div>
-                                        
-                    <h2 class="acc_trigger"><a href="#toggle3"><i class="fa fa-question-circle pr-3"></i>Can I use FIT certificates to register other degree programmes?</a></h2>
-                    <div class="acc_container">
-                        <div class="block">
-                            <p class="">
-                                <ul>
-                                    <li>At the moment, FIT is considered as an alternative qualification to register BIT programme.However, it is difficult to say about other programmes since conditions of registration vary from one to another. However, if any other institute decides to consider FIT certification equivalent qualification for registration, we will announce them.</li>
-                                </ul>
-                            </p>
-                        </div>
-                    </div>
-            
-                    <h2 class="acc_trigger"><a href="#toggle4"><i class="fa fa-question-circle pr-3"></i>When can I collect the certificates?</a></h2>
-                    <div class="acc_container">
-                        <div class="block">
-                            <p class="">
-                                <ul>
-                                    <li>When you have completed relevant e-Tests or practical tests successfully, you can apply to obtain a formal certificate by paying Rs. 500. All requests received before the first Monday of the last week of a month will be processed and the certificate will be issued during the first week of next month.</li>
-                                </ul>
-                            </p>
-                        </div>
-                    </div>
-
-                    <h2 class="acc_trigger"><a href="#toggle5"><i class="fa fa-question-circle pr-3"></i>How can I find more details about the FIT programme?</a></h2>
-                    <div class="acc_container">
-                        <div class="block">
-                            <p class="">
-                                <ul>
-                                    <li>FIT website could be a good starting point <a href="{{ url('/programme') }}"></a> http://fit.bit.lk . You can also download the student handbook from here</li>
-                                </ul>
-                            </p>
-                        </div>
+                {{-- QUESTION 1 --}}
+                <div class="card border-0">
+                    <div class="card-header bg-transparent border-bottom-0" id="headingOne">
+                        <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            <h4 class="mb-0"><i class="fa fa-question-circle pr-3"></i>Why should I follow FIT?</h4>
+                        </button>
                     </div>
                 
-            </div>
-        </section>
-        <!-- .// Accordion -->
+                    <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+                        <div class="card-body pt-0 pt-0 pl-md-5">
+                            It is a pre-degree program for those who wish to follow the <a href="http://www.bit.lk"><b>Bachelor of Information Technology (BIT)</b></a> at UCSC. 
+                            Moreover, it will be an added qualification for those who seek higher education in ICT or employment in the IT industry or BPO industry.
+                        </div>
+                    </div>
+                </div>
+                {{-- //QUESTION 1 --}}
 
+                {{-- QUESTION 2 --}}
+                <div class="card border-0">
+                    <div class="card-header bg-transparent border-bottom-0" id="headingTwo">
+                        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+                            <h4 class="mb-0"><i class="fa fa-question-circle pr-3"></i>What are the certificates that I can collect by following FIT?</h4>
+                        </button>
+                    </div>
+                
+                    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+                        <div class="card-body pt-0 pl-md-5">
+                            There are two certificates. When you pass all three e-Tests of FIT courses, you can get a certificate certifying your scores.
+                            <br/>When you complete all three e-Tests and two practical tests of FIT courses, you can get the final FIT certificate.
+                        </div>
+                    </div>
+                </div>
+                {{-- //QUESTION 2 --}}
+
+                {{-- QUESTION 3 --}}
+                <div class="card border-0">
+                    <div class="card-header bg-transparent border-bottom-0" id="headingThree">
+                        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
+                            <h4 class="mb-0"><i class="fa fa-question-circle pr-3"></i>Why should I follow FIT before I register BIT?</h4>
+                        </button>
+                    </div>
+                
+                    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+                        <div class="card-body pt-0 pl-md-5">
+                            You can obtain ICT related certification to find an employment in ICT or related areas as a beginner. Since BIT is an external degree programme, you could do a full time or part time job while following the <a href="http://www.bit.lk"><b>BIT Programme</b></a>
+                            <br/>If you do not have enough qualifications to register BIT programme, you can use FIT certification to obtain BIT registration.
+                        </div>
+                    </div>
+                </div>
+                {{-- //QUESTION 3 --}}
+
+                {{-- QUESTION 4 --}}
+                <div class="card border-0">
+                    <div class="card-header bg-transparent border-bottom-0" id="headingFour">
+                        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseFour" aria-expanded="true" aria-controls="collapseFour">
+                            <h4 class="mb-0"><i class="fa fa-question-circle pr-3"></i>Can I use FIT certificates to register other degree programmes?</h4>
+                        </button>
+                    </div>
+                
+                    <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordion">
+                        <div class="card-body pt-0 pl-md-5">
+                            At the moment, FIT is considered as an alternative qualification to register BIT programme.However, it is difficult to say about other programmes since conditions of registration vary from one to another. However, if any other institute decides to consider FIT certification equivalent qualification for registration, we will announce them.
+                        </div>
+                    </div>
+                </div>
+                {{-- //QUESTION 4 --}}
+
+                {{-- QUESTION 5 --}}
+                <div class="card border-0">
+                    <div class="card-header bg-transparent border-bottom-0" id="headingFive">
+                        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseFive" aria-expanded="true" aria-controls="collapseFive">
+                            <h4 class="mb-0"><i class="fa fa-question-circle pr-3"></i>When can I collect the certificates?</h4>
+                        </button>
+                    </div>
+                
+                    <div id="collapseFive" class="collapse" aria-labelledby="headingFive" data-parent="#accordion">
+                        <div class="card-body pt-0 pl-md-5">
+                            When you have completed relevant e-Tests or practical tests successfully, you can apply to obtain a formal certificate by paying Rs. 500. All requests received before the first Monday of the last week of a month will be processed and the certificate will be issued during the first week of next month.
+                        </div>
+                    </div>
+                </div>
+                {{-- //QUESTION 5 --}}
+
+                {{-- QUESTION 6 --}}
+                <div class="card border-0">
+                    <div class="card-header bg-transparent border-bottom-0" id="headingSix">
+                        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseSix" aria-expanded="true" aria-controls="collapseSix">
+                            <h4 class="mb-0"><i class="fa fa-question-circle pr-3"></i>How can I find more details about the FIT programme?</h4>
+                        </button>
+                    </div>
+                
+                    <div id="collapseSix" class="collapse" aria-labelledby="headingSix" data-parent="#accordion">
+                        <div class="card-body pt-0 pl-md-5">
+                            <a href="{{ url('/programme') }}"><b>Programme Structure</b></a> page could be a good starting point.
+                        </div>
+                    </div>
+                </div>
+                {{-- //QUESTION 6 --}}
+
+            </div>
+
+        </section>
+        {{-- //FAQ --}}
 
     </main>
 
