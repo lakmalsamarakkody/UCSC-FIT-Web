f815aa7b06fadd6e08c1fb61e5f78f7ba2256a9b
dinukolla <dinukolla@.github.com>
2020-11-24 13:04:06 +0530

    data migration

--- database/migrations/2020_11_24_051912_add_deleted_at_to_users_table.php
+++ database/migrations/2020_11_24_051912_add_deleted_at_to_users_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class AddDeletedAtToUsersTable extends Migration
@@ -16,6 +17,42 @@ class AddDeletedAtToUsersTable extends Migration
         Schema::table('users', function (Blueprint $table) {
             $table->softDeletes();
         });
+
+        DB::table('users')->insert(
+            array(
+                ['created_at'=> '2020-11-23 10:13:53',
+                'updated_at'=> '2020-11-23 10:13:53',
+                'name'=> 'dinusha',
+                'email'=>"kdd@ucsc.cm.ac.lk",
+                'password'=>'$2y$10$chLDig2zqSahPLNRnur.0O3xzyvjlzH54Jwsp.gblzj3Ltp.dfxkG'],
+
+                
+                ['created_at'=> '2020-11-23 10:13:53',
+                'updated_at'=> '2020-11-23 10:13:53',
+                'name'=> 'lakmal',
+                'email'=>"sls@ucsc.cmb.ac.lk",
+                'password'=>'$2y$10$chLDig2zqSahPLNRnur.0O3xzyvjlzH54Jwsp.gblzj3Ltp.dfxkG'],
+
+                ['created_at'=> '2020-11-23 10:13:53',
+                'updated_at'=> '2020-11-23 10:13:53',
+                'name'=> 'dhananga',
+                'email'=>"pdv@ucsc.cmb.ac.lk",
+                'password'=>'$2y$10$chLDig2zqSahPLNRnur.0O3xzyvjlzH54Jwsp.gblzj3Ltp.dfxkG'],
+
+                ['created_at'=> '2020-11-23 10:13:53',
+                'updated_at'=> '2020-11-23 10:13:53',
+                'name'=> 'yasindu',
+                'email'=>"yde@ucsc.cmb.ac.lk",
+                'password'=>'$2y$10$chLDig2zqSahPLNRnur.0O3xzyvjlzH54Jwsp.gblzj3Ltp.dfxkG'],
+                
+                ['created_at'=> '2020-11-23 10:13:53',
+                'updated_at'=> '2020-11-23 10:13:53',
+                'name'=> 'dr.thushani',
+                'email'=>"taw@ucsc.cmb.ac.lk",
+                'password'=>'$2y$10$chLDig2zqSahPLNRnur.0O3xzyvjlzH54Jwsp.gblzj3Ltp.dfxkG']
+
+            )
+        );
     }
 
     /**
--- database/migrations/2020_11_24_060312_create_anouncements_table.php
+++ database/migrations/2020_11_24_060312_create_anouncements_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class CreateAnouncementsTable extends Migration
@@ -20,6 +21,30 @@ class CreateAnouncementsTable extends Migration
             $table->string('announcement');
             $table->string('link');
         });
+        DB::table('anouncements')->insert(
+            array(
+                ['created_at'=> '2020-11-23 11:52:09',
+                'updated_at'=> '2020-11-23 11:52:14',
+                'announcement'=> 'October Exam Results Released',
+                'link'=>"{{url('/portal/results')}}"],
+
+                ['created_at'=> '2020-11-24 11:53:52',
+                'updated_at'=> '2020-11-24 11:54:13',
+                'announcement'=> 'Applications Now Open for December Exams',
+                'link'=>"{{url('/portal/register')}}"],
+                
+                ['created_at'=> '2020-11-24 11:55:27',
+                'updated_at'=> '2020-11-24 11:55:31',
+                'announcement'=> 'IMPORTANT NOTICE – Communication During Curfew',
+                'link'=>"{{url('/notice')}}"],
+                
+                ['created_at'=> '2020-11-24 12:01:16',
+                'updated_at'=> '2020-11-24 12:01:20',
+                'announcement'=> 'Extensions of Deadlines of the FIT Programme',
+                'link'=>"{{url('/notice')}}"],
+
+            )
+        );
     }
 
     /**
