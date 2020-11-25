a2643074ed7448ef31575fe4ff153c358d6e9028
dinukolla <dinukolla@.github.com>
2020-11-25 16:37:50 +0530

    portal

--- resources/views/layouts/portal.blade.php
+++ resources/views/layouts/portal.blade.php
@@ -19,6 +19,7 @@
             <!-- FONT AWESOME -->   <link rel="stylesheet" href="{{ asset('lib/font-awesome/css/all.css') }}">
             <!-- LINE AWESOME -->   <link rel="stylesheet" href="{{ asset('lib/line-awesome/css/line-awesome.css') }}">
             <!-- ANIMATE -->        <link rel="stylesheet" href="{{ asset('lib/animate/animate.min.css') }}">
+            <!-- CUSTOM -->        <link rel="stylesheet" href="{{ asset('css/portal/portal.css') }}">
 
 
        <!-- JavaScript Libraries -->
@@ -42,67 +43,65 @@
 
     <!-- Page Wrapper -->
     <div id="wrapper">
-      <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-        <div class="container">
-            <a class="navbar-brand" href="{{ url('/') }}">
-                {{ config('app.name', 'Laravel') }}
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                <span class="navbar-toggler-icon"></span>
-            </button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav mr-auto">
-
-                </ul>
-
-                <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ml-auto">
-                    <!-- Authentication Links -->
-                    @guest
-                        @if (Route::has('login'))
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                        @endif
-                        
-                        @if (Route::has('register'))
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                            </li>
-                        @endif
-                    @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }}
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
-                                                 document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
-                    @endguest
-                </ul>
-            </div>
+      
+      <nav class="navbar navbar-expand-md shadow-sm">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="{{ url('/') }}"><h3>Foundation of Information Technology<br>
+           <small>University of Colombo School of Computing</small> </h3>
+          </a>
+          <span class="navbar-text text-white" style="padding-left:50%;">
+            {{ Auth::user()->name }}
+          </span>
+              <span class="navbar-text text-white">
+                {{ Auth::user()->name }}
+              </span>
         </div>
       </nav>
 
-      <main class="py-4">
+      <!-- <nav class="navbar navbar-light bg-light shadow-sm">
+        <a class="navbar-brand" href="{{ url('/') }}"><h3>Foundation of Information Technology<br>
+          <small>University of Colombo School of Computing</small> </h3>
+        </a>
+        <span class="navbar-text">
+          {{ Auth::user()->name }}
+        </span>
+      </nav> -->
+      <!-- The sidebar -->
+      <div class="sidebar">
+        <ul class="sidebar" style="height: 100%;">
+          <li>
+            <a href="#home">Dashboard</a>
+          </li>
+          <li>            
+            <a href="#news">Students</a>
+          </li>
+          <li>            
+            <a href="#news">Exams</a>
+          </li>
+          <li>            
+            <a href="#news">Results</a>
+          </li>
+          <li>            
+            <a href="#news">Users</a>
+          </li>
+          <li>            
+            <a href="#news">System</a>
+          </li>
+          <li style="position:fixed; bottom: 0; width:200px">
+            <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+              <i class="fa fa-power-off"></i>&nbsp;&nbsp;&nbsp;&nbsp;Logout
+            </a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">@csrf</form>
+          </li>
+          
+        </ul>
+      </div>
+
+      <main class="container pt-5">
         @yield('content')
       </main>
     </div>
-
-
-
 </body>
 </html>
 
