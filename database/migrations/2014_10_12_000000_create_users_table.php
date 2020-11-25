6abecc35d4a54dd4fb5f855de951914c8f4ae96d
lakmalsamarakkody <43323601+lakmalsamarakkody@users.noreply.github.com>
2020-11-25 11:22:49 +0530

    migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 class CreateUsersTable extends Migration
 {
@@ -19,9 +20,52 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('role');
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
+
+        DB::table('users')->insert(
+            array(
+                ['name'=> 'dinusha',
+                'email'=>"kdd@ucsc.cmb.ac.lk",
+                'password'=>'$2y$10$chLDig2zqSahPLNRnur.0O3xzyvjlzH54Jwsp.gblzj3Ltp.dfxkG',
+                'role'=> 'admin',
+                'created_at'=> '2020-11-23 10:13:53',
+                'updated_at'=> '2020-11-23 10:13:53'],
+
+                
+                ['name'=> 'lakmal',
+                'email'=>"sls@ucsc.cmb.ac.lk",
+                'password'=>'$2y$10$chLDig2zqSahPLNRnur.0O3xzyvjlzH54Jwsp.gblzj3Ltp.dfxkG',
+                'role'=> 'admin',
+                'created_at'=> '2020-11-23 10:13:53',
+                'updated_at'=> '2020-11-23 10:13:53'],
+
+                ['name'=> 'dhananga',
+                'email'=>"pdv@ucsc.cmb.ac.lk",
+                'password'=>'$2y$10$chLDig2zqSahPLNRnur.0O3xzyvjlzH54Jwsp.gblzj3Ltp.dfxkG',
+                'role'=> 'admin',
+                'created_at'=> '2020-11-23 10:13:53',
+                'updated_at'=> '2020-11-23 10:13:53'],
+
+                ['name'=> 'yasindu',
+                'email'=>"yde@ucsc.cmb.ac.lk",
+                'password'=>'$2y$10$chLDig2zqSahPLNRnur.0O3xzyvjlzH54Jwsp.gblzj3Ltp.dfxkG',
+                'role'=> 'student',
+                'created_at'=> '2020-11-23 10:13:53',
+                'updated_at'=> '2020-11-23 10:13:53'],
+                
+                ['name'=> 'dr.thushani',
+                'email'=>"taw@ucsc.cmb.ac.lk",
+                'password'=>'$2y$10$chLDig2zqSahPLNRnur.0O3xzyvjlzH54Jwsp.gblzj3Ltp.dfxkG',
+                'role'=> 'admin',
+                'created_at'=> '2020-11-23 10:13:53',
+                'updated_at'=> '2020-11-23 10:13:53']
+
+            )
+        );
     }
 
     /**
--- database/migrations/2020_11_24_060312_create_anouncements_table.php
+++ database/migrations/2020_11_24_060312_create_anouncements_table.php
@@ -16,33 +16,38 @@ class CreateAnouncementsTable extends Migration
     {
         Schema::create('anouncements', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('description')->nullable();
+            $table->string('link')->nullable();
             $table->timestamps();
             $table->softDeletes();
-            $table->string('announcement');
-            $table->string('link');
         });
+
         DB::table('anouncements')->insert(
             array(
-                ['created_at'=> '2020-11-23 11:52:09',
-                'updated_at'=> '2020-11-23 11:52:14',
-                'announcement'=> 'October Exam Results Released',
-                'link'=>"{{url('/portal/results')}}"],
+                ['title'=> 'October Exam Results Released',
+                'description'=> '',
+                'link'=>"{{url('/portal/results')}}",
+                'created_at'=> '2020-11-23 10:13:53',
+                'updated_at'=> '2020-11-23 10:13:53'],
 
-                ['created_at'=> '2020-11-24 11:53:52',
-                'updated_at'=> '2020-11-24 11:54:13',
-                'announcement'=> 'Applications Now Open for December Exams',
-                'link'=>"{{url('/portal/register')}}"],
+                ['title'=> 'Applications Now Open for December Exams',
+                'description'=> 'Deadline on 31st of December',
+                'link'=>"{{url('/portal/register')}}",
+                'created_at'=> '2020-11-23 10:13:53',
+                'updated_at'=> '2020-11-23 10:13:53'],
                 
-                ['created_at'=> '2020-11-24 11:55:27',
-                'updated_at'=> '2020-11-24 11:55:31',
-                'announcement'=> 'IMPORTANT NOTICE – Communication During Curfew',
-                'link'=>"{{url('/notice')}}"],
+                ['title'=> 'IMPORTANT NOTICE – Communication During Curfew',
+                'description'=> '',
+                'link'=>"{{url('/notice')}}",
+                'created_at'=> '2020-11-23 10:13:53',
+                'updated_at'=> '2020-11-23 10:13:53'],
                 
-                ['created_at'=> '2020-11-24 12:01:16',
-                'updated_at'=> '2020-11-24 12:01:20',
-                'announcement'=> 'Extensions of Deadlines of the FIT Programme',
-                'link'=>"{{url('/notice')}}"],
-
+                ['title'=> 'Extensions of Deadlines of the FIT Programme',
+                'description'=> 'Deadline on 31st of December',
+                'link'=>"{{url('/notice')}}",
+                'created_at'=> '2020-11-23 10:13:53',
+                'updated_at'=> '2020-11-23 10:13:53'],
             )
         );
     }
