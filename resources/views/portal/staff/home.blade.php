0fde7c86f42e256e9e2ad1aa13b66ead6ba22193
dinukolla <dinukolla@.github.com>
2020-11-26 13:11:24 +0530

    Update home.blade.php

--- resources/views/portal/staff/home.blade.php
+++ resources/views/portal/staff/home.blade.php
@@ -9,7 +9,7 @@
                 <h1 class="m-0 text-dark">Dashboard</h1>
             </div><!-- /.col -->
             <div class="col-sm-6">
-                <ol class="breadcrumb float-sm-right">
+                <ol class="breadcrumb ">
                 <li class="breadcrumb-item"><a href="/home">Dashboard</a></li>
                 </ol>
             </div><!-- /.col -->
