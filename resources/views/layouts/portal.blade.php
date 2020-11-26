8454fa8f814d19452dd8a3a0aa743bd3cea0727f
lakmalsamarakkody <43323601+lakmalsamarakkody@users.noreply.github.com>
2020-11-26 22:49:23 +0530

    logout button style

--- resources/views/layouts/portal.blade.php
+++ resources/views/layouts/portal.blade.php
@@ -94,21 +94,9 @@
                 <li><a href="#789">System</a></li>
               </ul>
 
-              {{-- <div style="position:fixed; bottom: 0px;">
-                  <a class="text-white" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><div class="text-center logout pb-2"><i class="fa fa-power-off" style="font-size: 22px;"></i></div></a>
-                  <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">@csrf</form>
-              </div> --}}
-
-              {{-- <div class="logout-menu w-100 text-center bg-dark" style="position:relative; height:90%">
-                <div class="bg-secondary py-3" style="position: absolute; bottom: 0; left: 0; width: 100%;">
-                  <a class="text-white" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><i class="fa fa-power-off"></i>Logout</a>
-                  <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">@csrf</form>
-                </div>
-              </div> --}}
-
-              <div class="logout-menu w-100 text-center bg-dark" style="display:flex; align-items:flex-end;">
-                <div class="bg-secondary py-3" style="position: absolute; bottom: 0; left: 0; width: 100%;">
-                  <a class="text-white" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><i class="fa fa-power-off"></i>Logout</a>
+              <div class="w-100 text-center" style="display:flex; align-items:flex-end;">
+                <div class="logout-menu py-3" style="position: absolute; bottom: 0; left: 0; width: 100%;">
+                  <a class="text-white" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><i class="fa fa-power-off"></i> Logout</a>
                   <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">@csrf</form>
                 </div>
               </div>
