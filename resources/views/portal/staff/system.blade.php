35106c894a57c8d7df8faad5ae3c30657dc2b28f
lakmalsamarakkody <43323601+lakmalsamarakkody@users.noreply.github.com>
2020-11-27 13:52:31 +0530

    system cards complete

--- resources/views/portal/staff/system.blade.php
+++ resources/views/portal/staff/system.blade.php
@@ -26,11 +26,42 @@
     <!-- /BREACRUMB -->
 
     <!-- CONTENT -->
-    <div class="col-lg-12 system">
+    <div class="col-xl-12 system">
       <div class="row">
 
+        <!-- USER ROLE -->
+        <div class="col-xl-4 col-lg-12">
+          <div class="card border-0 shadow">
+            <div class="card-header">USER ROLE</div>
+            <div class="card-body">
+              <div class="card-text">
+                <table class="table table-responsive-lg">
+                  <thead>
+                    <tr>
+                      <th>Role Name</th>
+                      <th>&nbsp;</th>
+                    </tr>
+                  </thead>
+                  <thead>
+                    <tr>
+                      <td>System Administrator</td>
+                      <td class="text-right">
+                        <button type="button" class="btn btn-outline-success"><i class="fas fa-eye"></i></button>
+                        <button type="button" class="btn btn-outline-warning"><i class="fas fa-edit"></i></button>
+                        <button type="button" class="btn btn-outline-danger"><i class="fas fa-trash-alt"></i></button>
+                      </td>
+                    </tr>
+                  </thead>
+                </table>
+              </div>
+            </div>
+            <div class="card-footer text-right border-top-0"><button type="button" class="btn btn-outline-primary"><i class="fas fa-plus"></i></button></div>
+          </div>
+        </div>
+        <!-- /USER ROLE -->
+
         <!-- SUBJECT -->
-        <div class="col-lg-6 col-md-12">
+        <div class="col-xl-4 col-lg-12">
           <div class="card border-0 shadow">
             <div class="card-header">SUBJECTS</div>
             <div class="card-body">
@@ -56,13 +87,13 @@
                 </table>
               </div>
             </div>
-            <div class="card-footer text-right border-top-0"><button type="button" class="btn btn-outline-primary">Add Subject</button></div>
+            <div class="card-footer text-right border-top-0"><button type="button" class="btn btn-outline-primary"><i class="fas fa-plus"></i></button></div>
           </div>
         </div>
         <!-- /SUBJECT -->
 
         <!-- EXAM TYPE -->
-        <div class="col-lg-6 col-md-12">
+        <div class="col-xl-4 col-lg-12">
           <div class="card border-0 shadow">
             <div class="card-header">EXAM TYPES</div>
             <div class="card-body">
@@ -86,7 +117,7 @@
                 </table>
               </div>
             </div>
-            <div class="card-footer text-right border-top-0"><button type="button" class="btn btn-outline-primary">Add Subject</button></div>
+            <div class="card-footer text-right border-top-0"><button type="button" class="btn btn-outline-primary"><i class="fas fa-plus"></i></button></div>
           </div>
         </div>
         <!-- /EXAM TYPE -->
