6785b9946783e186a9b83406279fd412cfe3d3bb
lakmalsamarakkody <43323601+lakmalsamarakkody@users.noreply.github.com>
2020-11-30 12:02:57 +0530

    logout button revert

--- resources/views/layouts/portal.blade.php
+++ resources/views/layouts/portal.blade.php
@@ -101,7 +101,7 @@
                
               </ul>
 
-              <div style="position:fixed; bottom: 0px;" >
+              {{-- <div style="position:fixed; bottom: 0px;" >
                 <div class="text-center">
                   <a class="text-white" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                     <div class="logout pb-2" title="Logout" data-toggle="tooltip" data-placement="bottom" >
@@ -110,7 +110,14 @@
                   </a>
                   <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">@csrf</form>
                 </div>
-              </div> 
+              </div>  --}}
+
+              <div class="logout-menu w-100 text-center bg-dark" style="display:flex; align-items:flex-end;">
+                <div class="bg-secondary py-3" style="position: absolute; bottom: 0; left: 0; width: 100%;">
+                  <a class="text-white" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><i class="fa fa-power-off"></i>Logout</a>
+                  <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">@csrf</form>
+                </div>
+              </div>
               
 
             </div>
