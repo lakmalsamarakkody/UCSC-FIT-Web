70c46935d8e59d03ac321f0330a2a7ba58473d3c
lakmalsamarakkody <43323601+lakmalsamarakkody@users.noreply.github.com>
2020-12-01 17:21:27 +0530

    datatables function removed

--- app/Http/Controllers/Portal/Staff/ExamsController.php
+++ app/Http/Controllers/Portal/Staff/ExamsController.php
@@ -4,35 +4,18 @@ namespace App\Http\Controllers\Portal\Staff;
 
 use App\Http\Controllers\Controller;
 use App\Models\Exam;
-use Yajra\Datatables\Facades\DataTables;
 use Illuminate\Http\Request;
 
 class ExamsController extends Controller
 {    
-    public function __construct()
-    {
-        $this->middleware('auth');
-        $this->middleware('revalidate');
-    }
-    
-    public function index()
-    {
-        return view('portal/staff/exams');
-    }
-
-    public function getExamList(Request $request)
-    {
-        if ($request->ajax()) {
-            $data = Exam::latest()->get();
-            return Datatables::of($data)
-            ->addIndexColumn()
-            ->addColumn('action', function($row){
-                $actionBtn = '<a href="javascript:void(0)" class="edit btn btn-success btn-sm">Edit</a> 
-                <a href="javascript:void(0)" class="delete btn btn-danger btn-sm">Delete</a>';
-                return $actionBtn;
-            })
-            ->rawColumns(['sction'])
-            ->make(true);
-        }
-    }
+  public function __construct()
+  {
+      $this->middleware('auth');
+      $this->middleware('revalidate');
+  }
+  
+  public function index()
+  {
+      return view('portal/staff/exams');
+  }
 }
