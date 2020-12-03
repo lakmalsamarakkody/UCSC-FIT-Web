35f5577e378f64541be3ff6bfd7da281d8dfa72c
dinukolla <dinukolla@.github.com>
2020-12-03 12:40:12 +0530

    Update web.blade.php

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -150,7 +150,7 @@
                     Copyright &copy;  {{ now()->year }}<strong><a target="_blank" href="https://ucsc.cmb.ac.lk/" class="white"> UCSC</a> </strong>. All Rights Reserved 
                 </div>
                 <div class="credits">
-                    Designed by <strong><a target="_blank" href="http://www.e-learning.lk/" class="white">e-Learning Center- UCSC </a> </strong>
+                    Powered by <strong><a target="_blank" href="http://www.e-learning.lk/" class="white">e-Learning Center- UCSC </a> </strong>
                 </div>
             </div>
         </div>
