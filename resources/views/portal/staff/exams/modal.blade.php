008594b4dad80f90a5b328ef819d0316b5982ce6
Parakramaba <74170793+Parakramaba@users.noreply.github.com>
2020-12-04 18:22:54 +0530

    add endTime

--- resources/views/portal/staff/exams/modal.blade.php
+++ resources/views/portal/staff/exams/modal.blade.php
@@ -28,13 +28,17 @@
                               <option value="">Practical</option>
                             </select>
                           </div>
-                          <div class="form-group col-xl-3 col-lg-6">
+                          <div class="form-group col-xl-2 col-lg-4">
                             <label for="examDate">Date</label>
-                            <input type="date" name="examDate" class="form-control"/>
+                            <input type="date" name="examDate" id="examDate" class="form-control"/>
                           </div>
-                          <div class="form-group col-xl-2 col-lg-6">
+                          <div class="form-group col-xl-2 col-lg-4">
                             <label for="startTime">Start Time</label>
-                            <input type="time" name="startTime" class="form-control"/>
+                            <input type="time" name="startTime" id="startTime" class="form-control"/>
+                          </div>
+                          <div class="form-group col-xl-2 col-lg-4">
+                            <label for="endTime">End Time</label>
+                            <input type="time" name="endTime" id="endTime" class="form-control"/>
                           </div>
                         </div>
                     </form>
@@ -60,7 +64,7 @@
         <div class="modal-dialog modal-dialog-centered modal-xl">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="postponeExamLabel">Apply Changes</h5>
+                    <h5 class="modal-title" id="postponeExamLabel">Postpone Exam</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
@@ -68,13 +72,17 @@
                 <div class="modal-body">
                     <form>
                         <div class="form-row align-items-center">
-                            <div class="form-group col-6">
+                            <div class="form-group col-4">
                                 <label for="examDate">Date</label>
-                                <input type="date" class="form-control" name="examDate" id="examDate">
+                                <input type="date" class="form-control" name="examDate" id="examDate" />
                             </div>
-                            <div class="form-group col-6">
+                            <div class="form-group col-4">
                                 <label for="startTime">Start Time</label>
-                                <input type="time" class="form-control" name="startTime" id="startTime">
+                                <input type="time" class="form-control" name="startTime" id="startTime" />
+                            </div>
+                            <div class="form-group col-4">
+                                <label for="endTime">End Time</label>
+                                <input type="time" class="form-control" name="endTime" id="endTime" />
                             </div>
 
                         </div>
