21a1fb4f42d21eb90fc7adfa4d10712decc8a24d
dinukolla <dinukolla@.github.com>
2020-12-08 16:36:42 +0530

    Update ExamScheduleSeeder.php

--- database/seeders/ExamScheduleSeeder.php
+++ database/seeders/ExamScheduleSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Exam;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -15,28 +16,30 @@ class ExamScheduleSeeder extends Seeder
     public function run()
     {
       $faker=\Faker\Factory::create();
-          
-      for ($i=0; $i < 20; $i++):
-        $Sid = $faker->numberBetween($min = 1, $max = 3);
-        if( $Sid == 3):
-          $Etid = 1;
-        else:
-          $Etid = $faker->numberBetween($min = 1, $max = 2);
-        endif;
-        DB::table('exam_schedules')->insert(
-          array( 
-              [
-                'exam_id' => $faker->numberBetween($min = 1, $max = 10),
-                'subject_id' =>$Sid,
-                'exam_type_id' => $Etid,
-                'date' => $faker->dateTimeBetween('2020-01-01', '2020-12-31'),
-                'start_time' => $faker->time(),
-                'end_time' => $faker->time(),
-                'created_at' => '2020-11-27 17:36:23',
-                'updated_at' => '2020-11-27 17:36:23'
-              ]
-          )         
-      );
-      endfor;
+      $exam_ids=Exam::all();
+      foreach ($exam_ids as $exam_id):
+        for ($i=0; $i < 20; $i++):
+          $Sid = $faker->numberBetween($min = 1, $max = 3);
+          if( $Sid == 3):
+            $Etid = 1;
+          else:
+            $Etid = $faker->numberBetween($min = 1, $max = 2);
+          endif;
+          DB::table('exam_schedules')->insert(
+            array( 
+                [
+                  'exam_id' => $exam_id->id,
+                  'subject_id' =>$Sid,
+                  'exam_type_id' => $Etid,
+                  'date' => $faker->dateTimeBetween($exam_id->year .'-'. $exam_id->month.'-01', '2020-12-31'),
+                  'start_time' => $faker->time(),
+                  'end_time' => $faker->time(),
+                  'created_at' => '2020-11-27 17:36:23',
+                  'updated_at' => '2020-11-27 17:36:23'
+                ]
+            )         
+        );
+        endfor;
+      endforeach;
     }
 }
