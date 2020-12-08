1d5f0bbac83989eaec06ccd01353f0899d1a0c15
dinukolla <dinukolla@.github.com>
2020-12-08 11:07:22 +0530

    Create 2020_12_08_041730_create_result_months_table.php

---
+++ database/migrations/2020_12_08_041730_create_result_months_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateResultMonthsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('result_months', function (Blueprint $table) {
+            $table->id();
+            $table->year('year');
+            $table->string('month');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('result_months');
+    }
+}
