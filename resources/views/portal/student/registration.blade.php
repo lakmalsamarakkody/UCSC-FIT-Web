fe4721ec42714787b89796e7eb796c983c59de3a
Parakramaba <74170793+Parakramaba@users.noreply.github.com>
2020-12-08 16:19:40 +0530

    Personal Details

--- resources/views/portal/student/registration.blade.php
+++ resources/views/portal/student/registration.blade.php
@@ -10,6 +10,16 @@
     });
 
 </script>
+
+<!--
+<script type="text/javascript">
+    // SHOW INPUT FILED IN TITLE
+    function showfield(name){
+      if(name=='Other')document.getElementById('oth').innerHTML='<input type="text" class="form-control" name="other" />';
+      else document.getElementById('oth').innerHTML='';
+    }
+</script> -->
+
     <!-- BREACRUMB -->
     <section class="col-sm-12 mb-3">
         <div class="row">
@@ -30,8 +40,77 @@
         <div class="row">
             <div class="col-12 mt-5">
                 <div class="card">
-                    <div class="card-header text-center">Register to FIT</div>
+                    <div class="card-header text-center">Register to FIT <br><small style="color:slategrey; text-transform: initial;">Please fill all the details correctly</small></div>
                     <div class="card-body">
+                        <form>
+                            <div class="details px-3">
+                                <h6 class="text-left mt-4 mb-5">Personal Details</h6>
+                                <div class="form-row align-item-center">
+                                    <div class="form-group col-xl-6 col-md-12">
+                                        <label for="firstName">First Name</label>
+                                        <input type="text" class="form-control" id="firstName" name="firstName" placeholder="Kamal"/>
+                                    </div>
+                                    <div class="from-group col-xl-6 col-md-12">
+                                        <label for="lastName">Last Name</label>
+                                        <input type="text" class="form-control" id="lastName" name="lastName" placeholder="Atthanayke">
+                                    </div>
+                                    <div class="form-group col-xl-6 col-md-12">
+                                        <label for="otherName">Other Name</label>
+                                        <input type="text" class="form-control" id="otherName" name="otherName" placeholder="Kankanange">
+                                    </div>
+                                    <div class="form-group col-xl-6 col-md-12">
+                                        <label for="nameInitials">Name with Initials</label>
+                                        <input type="text" class="form-control" id="nameInitials" name="nameInitials" placeholder="K K Atthanayke">
+                                    </div>
+                                    <div class="form-group col-xl-6 col-md-12">
+                                        <label for="title">Title</label>
+                                        <select name="title" id="title" onchange="showfield(this.options[this.selectedIndex].value)" class="form-control">
+                                            <option value="">Rev</option>
+                                            <option value="">Dr</option>
+                                            <option value="" selected>Mr</option>
+                                            <option value="">Miss</option>
+                                            <option value="">Mrs</option>
+                                            <option value="">Other(Specify)</option>
+                                        </select>
+                                       <!-- <div class="col-xl-6 col-md-12" id="divOther"></div> -->
+                                    </div>
+                                    <div class="form-group col-xl-6 col-md-12">
+                                        <label for="gender">Gender</label>
+                                        <select name="gender" id="gender" class="form-control">
+                                            <option value="">Male</option>
+                                            <option value="">Female</option>
+                                        </select>
+                                    </div>
+                                    <div class="form-group col-xl-6 col-md-12">
+                                        <label for="citizenship">Citizenship</label>
+                                        <select name="citizenship" id="citizenship" class="form-control">
+                                            <option value="">Sri Lankan</option>
+                                            <option value="">Foreign National</option>
+                                        </select>
+                                    </div>
+                                    <div class="form-group col-xl-6 col-md-12">
+                                        <label for="nic">Narional ID / Passport No.</label>
+                                        <input type="text" class="form-control" id="nic" name="nic" placeholder="960342704V">
+                                    </div>
+                                    <div class="form-group col-xl-6 col-md-12">
+                                        <label for="dob">Date of Birth</label>
+                                        <input type="date" class="form-control" id="dob" name="dob">
+                                    </div>
+                                    <div class="form-group col-xl-6 col-md-12">
+                                        <label for="qulification">Highest Qualification</label>
+                                        <select name="qulification" id="qulification" class="form-control">
+                                            <option value="">Bachelor's Degree</option>
+                                            <option value="">GCE Advanced Level</option>
+                                            <option value="">GCE Ordinary Level</option>
+                                        </select>
+                                    </div>
+
+
+
+
+                            </div>
+                        </div>
+                        </form>
 
                     </div>
                 </div>
