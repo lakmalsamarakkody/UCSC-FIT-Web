de3ed5ab5c7ff269f69a9ba45cda34ce611f4d53
dinukolla <dinukolla@.github.com>
2020-12-08 18:55:17 +0530

    Update ResultsController.php

--- app/Http/Controllers/Portal/Staff/ResultsController.php
+++ app/Http/Controllers/Portal/Staff/ResultsController.php
@@ -5,7 +5,10 @@ namespace App\Http\Controllers\Portal\Staff;
 use App\Http\Controllers\Controller;
 use App\Models\Student_Exam;
 use App\Models\Exam;
+use App\Models\Exam\Schedule;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use SebastianBergmann\Environment\Console;
 
 class ResultsController extends Controller
 {
@@ -17,14 +20,23 @@ class ResultsController extends Controller
     
     public function index()
     {
-        $months = Exam::latest();
-        $months = $months->paginate(7);
-        return view('portal/staff/results',compact('months'));
+        $exams = Exam::latest();
+        $exams = $exams->paginate(10);
+        $years = Exam::select('year')->groupBy('year')->get();
+        $months = Exam::select('month')->groupBy('month')->get();
+        return view('portal/staff/results',compact('exams','years','months'));
     }
 
     public function viewResults($id)
     {
-        // $results=Student_Exam::where('result_id',$id)->get();
-        // return view('portal/staff/result/view', compact('results'));
+        $schedules=Schedule::where('exam_id',$id)->get();
+        $results = array();
+        foreach($schedules as $schedule){
+            $results[]=Student_Exam::where('exam_schedule_id',$schedule->id);
+        }
+        $results = collect($results);
+        $results=$results->groupBy('student_id');
+        Log::debug($results);
+        return view('portal/staff/result/view', compact('results'));
     }
 }
