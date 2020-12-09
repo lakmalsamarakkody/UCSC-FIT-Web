bd937d06e33cdfff43cc9c78e4630a929e01ece9
lakmalsamarakkody <43323601+lakmalsamarakkody@users.noreply.github.com>
2020-12-09 16:59:05 +0530

    student result page

--- resources/views/portal/student/registration/scripts.blade.php
+++ resources/views/portal/student/registration/scripts.blade.php
@@ -15,19 +15,16 @@
     document.getElementById('InputRoleName').setAttribute("disabled","disabled");
   } */
 
-  enter_current_addressss = () => {
-    if($('#addrs').attr('checked')){
+  address_editable = () => {
+    console.log('hello');
+    if(document.getElementById("current_address").checked == true) {
+      console.log('disabled');
       $('#currentHouse').removeAttr('disabled');
     }
     else{
       document.getElementById('currentHouse').setAttribute("disabled","disabled");
     }
   }
-  function current_address(addrs) {
-    document.getElementById("currentHouse").disabled = addrs.checked ? false:true;
-    if(!document.getElementById("currentHouse").disabled){
-      document.getElementById("currentHouse").focus;
-    }
-  }
+  
   // /INSERT CURRENT ADDRESS
   </script>
--- resources/views/portal/student/results.blade.php
+++ resources/views/portal/student/results.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.portal')
+@extends('layouts.student_portal')
 
 @section('content')
 
@@ -14,78 +14,41 @@
     <!-- CONTENT -->
     <div class="col-lg-12 results">
       <div class="row">
-        <!-- <div class="col-lg-2"></div> -->
 
-        <div class="col-lg-12">
-
-            <div class="card results">
-              <div class="card-header">
-                Filters
-              </div>
-              <div class="card-body">
-                <form action="{{ route('students') }}" method="GET">
-                  <div class="form-row">
-                    <div class="form-group col-lg-3"></div>
-                    <div class="form-group col-lg-3">
-                      <select id="year" name="year" class="form-control ">
-                        <option selected>Year</option>
-                        @foreach($years as $year)                          
-                        <option value="{{ $year->year }}">{{ $year->year }}</option>
-                        @endforeach
-                      </select>
-                    </div>
-                    <div class="form-group col-lg-3">
-                      <select id="month" name="month" class="form-control ">
-                        <option selected>Month</option>
-                        @foreach($months as $month)                          
-                        <option value="{{ $month->month }}">{{ $month->month }}</option>
-                        @endforeach
-                      </select>
-                    </div>
-                    <div class="form-group col-lg-3"></div>
-                  </div>
-                </form>
-              </div>
-            </div>
-
-        </div>
-        <!-- <div class="col-lg-1"></div> -->
-        <div class="col-lg-12">
-          <div class="card shadow  mt-3">
-            <div class="card-header">
-              Results
-            </div>
-            <div class="card-body">
-              <div class="col-lg-12">
-                @foreach($exams as $exam)
-                <div class="card my-2">
-                  <div class="card-header align-middle">
-                    <div class="row">
-                      <div class="col-sm-8">
-  
-                        <h3 class="mb-0">{{ $exam->year }}&nbsp; {{ $exam->month }}</h5> 
-                      </div>
-                      <div class="col-sm-4">
-                        <a class="btn btn-outline-success w-100 text-center" href="{{ url('/portal/staff/results/view/') }}/{{ $exam->id }}" target="_blank">
-                          <i class="fa fa-eye"></i>
-                          &nbsp;View Results
-                        </a>
-                      </div>
-                    </div>
-
-                  </div>
-                </div>
-
-                  
-                @endforeach
-                <div class="pt-4 float-right">
-                  {{ $exams->links( "pagination::bootstrap-4") }}
-                </div>
-              </div>
-              
-            </div>
+        <div class="card col-12">
+          <div class="card-header">
+            Name : S A L SAMARAKKODY <br/>
+            Registration No : F021479136
           </div>
-
+          <div class="card-body">
+            <table class="table table-bordered table-responsive-md">
+              <thead class="text-center">
+                <tr>
+                  <th rowspan="2">Exam</th>
+                  <th colspan="2">FIT 103</th>
+                  <th colspan="2">FIT 203</th>
+                  <th colspan="2">FIT 303</th>
+                </tr>
+                <tr>
+                  <th>E-Test</th>
+                  <th>Practical</th>
+                  <th>E-Test</th>
+                  <th>Practical</th>
+                  <th>E-Test</th>
+                  <th>Practical</th>
+                </tr>
+              </thead>
+              <tbody>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tbody>
+            </table>
+          </div>
         </div>
 
       </div>
