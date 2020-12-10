a96c3b7af7ab0abc525ea83661754302e0b4d766
dinukolla <dinukolla@.github.com>
2020-12-10 17:28:28 +0530

    Update registration.blade.php

--- resources/views/portal/student/registration.blade.php
+++ resources/views/portal/student/registration.blade.php
@@ -20,25 +20,11 @@
     }
 </script> -->
 
-    <!-- BREACRUMB -->
-    <section class="col-sm-12 mb-3">
-        <div class="row">
-           
-          <nav aria-label="breadcrumb">
-            <ol class="breadcrumb ">
-              <li class="breadcrumb-item"><a href="{{ url('/portal/student/') }}">Dashboard</a></li>
-              <li class="breadcrumb-item active" aria-current="page">Registration</li>
-            </ol>
-          </nav>
-
-        </div>
-    </section>
-    <!-- /BREACRUMB -->
 
     <!-- CONTENT -->
-    <div class="col-12 student-registration">
+    <div class="col-lg-12 student-registration">
         <div class="row">
-            <div class="col-12 mt-5">
+            <div class="col-12 ">
                 <div class="card">
                     <div class="card-header text-center">Register to FIT Programme<br><small style="text-transform: initial;">Please fill all the details correctly</small></div>
                     <div class="card-body">
