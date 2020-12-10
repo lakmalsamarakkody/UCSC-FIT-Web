6a514370b54a7902fae8151b4715a8e644bf5e5a
Parakramaba <74170793+Parakramaba@users.noreply.github.com>
2020-12-10 10:05:27 +0530

    Enable/Disable edit current address

--- resources/views/portal/student/registration/scripts.blade.php
+++ resources/views/portal/student/registration/scripts.blade.php
@@ -18,11 +18,24 @@
   address_editable = () => {
     console.log('hello');
     if(document.getElementById("current_address").checked == true) {
-      console.log('disabled');
+      //console.log('disabled');
       $('#currentHouse').removeAttr('disabled');
+      $('#currentAddressLine1').removeAttr('disabled');
+      $('#currentAddressLine2').removeAttr('disabled');
+      $('#currentAddressLine3').removeAttr('disabled');
+      $('#currentAddressLine4').removeAttr('disabled');
+      $('#currentCity').removeAttr('disabled');
+      $('#plusCurrentField').removeAttr('disabled');
     }
     else{
       document.getElementById('currentHouse').setAttribute("disabled","disabled");
+      document.getElementById('currentAddressLine1').setAttribute("disabled","disabled");
+      document.getElementById('currentAddressLine2').setAttribute("disabled","disabled");
+      document.getElementById('currentAddressLine3').setAttribute("disabled","disabled");
+      document.getElementById('currentAddressLine4').setAttribute("disabled","disabled");
+      document.getElementById('currentCity').setAttribute("disabled","disabled");
+      document.getElementById('plusCurrentField').setAttribute("disabled","disabled");
+
     }
   }
   
