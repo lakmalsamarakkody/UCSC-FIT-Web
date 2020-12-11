73f396204fb8fda1d0f8376933079d6406bd081f
yasindudehan <ruwanwellayasindu@gmail.com>
2020-12-11 22:04:12 +0530

    examschedule

--- app/Http/Controllers/Mobileapp/StudentExamController.php
+++ app/Http/Controllers/Mobileapp/StudentExamController.php
@@ -6,11 +6,31 @@ use App\Http\Controllers\Controller;
 use App\Models\Exam as ModelsExam;
 use App\Models\Exam\Schedule as ExamSchedule;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Models\Student_Exam as StuExam;
+
+use App\Models\Subject;
 
 class StudentExamController extends Controller
 {
-    function currentExam(){
-        return ExamSchedule::all();
+
+    function examSchedule(){
+       return [
+        "status"=>true,
+        "Data"=>DB::table('exam_schedules')
+       ->join('subjects','exam_schedules.subject_id',"=",'subjects.id')
+       ->join('exam_types','exam_schedules.exam_type_id',"=",'exam_types.id')
+       ->join('exams','exam_schedules.exam_id',"=",'exams.id')
+       ->select(
+           'exam_schedules.date',
+           'exam_schedules.start_time',
+           'exam_schedules.end_time',
+           'subjects.code',
+           'subjects.name',
+           'exam_types.exam_type',
+           'exams.year',
+           'exams.month'
+           )
+       ->get()];
     }
 }
