685e318d067ac9ca8178ddf284d4cafb39eb38e5
lakmalsamarakkody <43323601+lakmalsamarakkody@users.noreply.github.com>
2020-12-14 11:16:51 +0530

    exam layout redesigned

--- resources/views/portal/student/exams.blade.php
+++ resources/views/portal/student/exams.blade.php
@@ -15,82 +15,90 @@
     <div class="col-lg-12 student-exams min-vh-100">
       <div class="row">
 
-        <!-- EXAM SCHEDULE TABLE -->
-        <div class="col-12 md-5 mt-4">
+        <!-- UPCOMING EXAM SCHEDULE -->
+        <div class="col-12 mt-4">
           <div class="card">
             <div class="card-header">Upcoming Schedules</div>
             <div class="card-body">
-              <table class="table yajra-datatable">
-                <thead class="text-center">
-                  <tr>
-                    <th>Subject Code</th>
-                    <th>Subject Name</th>
-                    <th>Exam Type</th>
-                    <th>Date</th>
-                    <th>Start Time</th>
-                    <th>End Time</th>
-                    <th>&nbsp;</th>
-                  </tr>
-                </thead>
-                <tbody>
+
+              <div class="card w-100 shadow-none border border-secondary">
+                <div class="card-header text-primary">2020 December</div>
+                <div class="card-body">
+                  {{-- <pre>
+                    {{$exams}}
+                  </pre> --}}
                   @foreach ($exams as $exam)
-                  <tr class="text-center">
-                    <td>FIT {{ $exam->subject->code }}</td>
-                    <td>{{ $exam->subject->name }}</td>
-                    <td>{{ $exam->type->exam_type }}</td>
-                    <td>{{ $exam->date }}</td>
-                    <td>10:30 AM</td>
-                    <td>12:30 PM</td>
-                    <td>
-                      <div class="btn-group">
-                        <button type="button" class="btn btn-outline-success" data-tooltip="tooltip" data-placement="bottom" title="Apply Exam" onclick="window.open('/portal/student/payment')"><i class="far fa-hand-point-right"></i></button>
+                  <div class="accordion" id="accordion_{{$exam->exam_id}}">
+                    <div class="card mb-4 shadow-sm">
+                      <div class="card-header text-secondary" id="heading_{{$exam->exam_id}}">
+                        FIT {{ $exam->subject->code }} - {{ $exam->subject->name }} ({{ $exam->type->exam_type }})
+                        <div class="btn-group float-right">
+                          <button class="btn btn-outline-success" type="button" data-toggle="collapse" data-target="#collapse_{{$exam->exam_id}}" aria-expanded="true" aria-controls="collapse_{{$exam->exam_id}}"><i class="far fa-eye"></i> View</button>
+                        </div>
                       </div>
-                    </td>
-                  </tr>
+                  
+                      <div id="collapse_{{$exam->exam_id}}" class="collapse" aria-labelledby="heading_{{$exam->exam_id}}" data-parent="#accordion_{{$exam->exam_id}}">
+                        <div class="card-body text-md-center border-top border-secondary">
+                          <div class="row">
+                            <div class="col-12 col-md-4"> Date : {{ $exam->date }}</div>
+                            <div class="col-12 col-md-4"> Start Time : {{ $exam->start_time }}</div>
+                            <div class="col-12 col-md-4"> End Time : {{ $exam->end_time }}</div>
+                            <div class="col-12 offset-md-4 col-md-4 my-3">
+                              <button type="button" class="btn btn-outline-primary w-100" data-tooltip="tooltip" data-placement="bottom" title="Apply Exam" onclick="window.open('/portal/student/payment')"><i class="far fa-hand-point-right"></i> Apply</button>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                   @endforeach
-                </tbody>
-              </table>
+                </div>
+              </div>
 
             </div>
           </div>
         </div>
-        <!-- /EXAM SCHEDULE TABLE-->
+        <!-- /UPCOMING EXAM SCHEDULE-->
 
         <!-- APPLIED EXAMS TABLE -->
-        <div class="col-12 md-5 mt-4">
+        <div class="col-12 mt-4">
           <div class="card">
             <div class="card-header">Applied Exams</div>
             <div class="card-body">
-              <table class="table yajra-datatable">
-                <thead class="text-center">
-                  <tr>
-                    <th>Subject Code</th>
-                    <th>Subject Name</th>
-                    <th>Exam Type</th>
-                    <th>Date</th>
-                    <th>Start Time</th>
-                    <th>End Time</th>
-                    <th>&nbsp;</th>
-                  </tr>
-                </thead>
-                <tbody>
+
+              <div class="card w-100 shadow-none border border-secondary">
+                <div class="card-header text-primary">2020 December</div>
+                <div class="card-body">
+                  {{-- <pre>
+                    {{$exams}}
+                  </pre> --}}
                   @foreach ($exams as $exam)
-                  <tr class="text-center">
-                    <td>FIT {{ $exam->subject->code }}</td>
-                    <td>{{ $exam->subject->name }}</td>
-                    <td>{{ $exam->type->exam_type }}</td>
-                    <td>{{ $exam->date }}</td>
-                    <td>10:30 AM</td>
-                    <td>12:30 PM</td>
-                    <td>
-                      <div class="btn-group">
-                        <button type="button" class="btn btn-outline-danger" data-tooltip="tooltip" data-placement="bottom" title="Cancel Exam"><i class="fas fa-times"></i></button>
+                  <div class="accordion" id="accordion_{{$exam->exam_id}}">
+                    <div class="card mb-4 shadow-sm">
+                      <div class="card-header text-secondary" id="heading_{{$exam->exam_id}}">
+                        FIT {{ $exam->subject->code }} - {{ $exam->subject->name }} ({{ $exam->type->exam_type }})
+                        <div class="btn-group float-right">
+                          <button class="btn btn-outline-success" type="button" data-toggle="collapse" data-target="#collapse_{{$exam->exam_id}}" aria-expanded="true" aria-controls="collapse_{{$exam->exam_id}}"><i class="far fa-eye"></i> View</button>
+                        </div>
                       </div>
-                    </td>
-                  </tr>
+                  
+                      <div id="collapse_{{$exam->exam_id}}" class="collapse" aria-labelledby="heading_{{$exam->exam_id}}" data-parent="#accordion_{{$exam->exam_id}}">
+                        <div class="card-body text-md-center border-top border-secondary">
+                          <div class="row">
+                            <div class="col-12 col-md-4"> Date : {{ $exam->date }}</div>
+                            <div class="col-12 col-md-4"> Start Time : {{ $exam->start_time }}</div>
+                            <div class="col-12 col-md-4"> End Time : {{ $exam->end_time }}</div>
+                            <div class="col-12 offset-md-4 col-md-4 my-3">
+                              <button type="button" class="btn btn-outline-danger w-100" data-tooltip="tooltip" data-placement="bottom" title="Cancel Exam"><i class="fas fa-times"></i> Cancel</button>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                   @endforeach
-                </tbody>
-              </table>
+                </div>
+              </div>
 
             </div>
           </div>
@@ -98,40 +106,27 @@
         <!-- /APPLIED EXAMS TABLE-->
 
         <!-- FACED EXAMS TABLE -->
-        <div class="col-12 md-5 mt-4">
+        <div class="col-12 mt-4">
           <div class="card">
             <div class="card-header">Faced Exams</div>
             <div class="card-body">
-              <table class="table yajra-datatable">
-                <thead class="text-center">
-                  <tr>
-                    <th>Subject Code</th>
-                    <th>Subject Name</th>
-                    <th>Exam Type</th>
-                    <th>Date</th>
-                    <th>Start Time</th>
-                    <th>End Time</th>
-                    <th>&nbsp;</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  @foreach ($exams as $exam)
-                  <tr class="text-center">
-                    <td>FIT {{ $exam->subject->code }}</td>
-                    <td>{{ $exam->subject->name }}</td>
-                    <td>{{ $exam->type->exam_type }}</td>
-                    <td>{{ $exam->date }}</td>
-                    <td>10:30 AM</td>
-                    <td>12:30 PM</td>
-                    <td>
-                      <div class="btn-group">
-                        <button type="button" class="btn btn-outline-danger" data-tooltip="tooltip" data-placement="bottom" title="Upload Medical"><i class="fas fa-file-medical"></i></button>
-                      </div>
-                    </td>
-                  </tr>
-                  @endforeach
-                </tbody>
-              </table>
+
+              <h5>2020 November</h5>
+
+              <ul class="list-group list-group-flush">
+                @foreach ($exams as $exam)
+                <li class="list-group-item">
+                  <div class="row">
+                    <div class="col-12 col-md-4">FIT {{ $exam->subject->code }}</div>
+                    <div class="col-12 col-md-4">{{ $exam->subject->name }} ({{ $exam->type->exam_type }})</div>
+                    <div class="col-12 col-md-4 text-md-right">
+                      <button type="button" class="btn btn-outline-danger w-100" data-tooltip="tooltip" data-placement="bottom" title="Upload Medical"><i class="fas fa-file-medical"></i> Upload medical</button>
+                    </div>
+                  </div>
+                  
+                </li>
+                @endforeach
+              </ul>
 
             </div>
           </div>
