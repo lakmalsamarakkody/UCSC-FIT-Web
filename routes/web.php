e2d4397fff6cb70fdfcc260b2166867a8ad2a89a
dinukolla <dinukolla@.github.com>
2020-12-17 00:46:29 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -34,8 +34,9 @@ Route::get('/faq',[App\Http\Controllers\Website\Faq::class, 'index']);
 Route::get('/announcements', [App\Http\Controllers\Website\AnouncementsController::class, 'index']);
 
 Route::post('/student/registration',[App\Http\Controllers\Website\Registration::class,'emailLink']);
-Route::get('/guest/{email}/fit/{token}', [App\Http\Controllers\Website\Registration::class,'updateAccount']);
+Route::get('/guest/{email}/fit/{token}', [App\Http\Controllers\Website\Registration::class,'updateAccount'])->name('email.link');
 Route::get('/website/error', [App\Http\Controllers\Website\Registration::class,'index']);
+
 /*
 |--------------------------------------------------------------------------
 | PORTAL ROUTES
