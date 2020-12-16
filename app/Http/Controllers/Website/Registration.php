54ff998d6df2b8d3e61f93f2feda8dbf7df1a870
dinukolla <dinukolla@.github.com>
2020-12-16 11:59:23 +0530

    Update Registration.php

--- app/Http/Controllers/Website/Registration.php
+++ app/Http/Controllers/Website/Registration.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers\Website;
 
 use App\Http\Controllers\Controller;
 use App\Mail\StudentRegistration;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
 
 class Registration extends Controller
 {
@@ -17,7 +19,8 @@ class Registration extends Controller
     public function emailLink(Request $request)
     {
         $validator = Validator::make($request->all(), [            
-            'email'=> ['required', 'email', 'unique:users']
+            'email'=> ['required', 'email']      
+            // 'email'=> ['required', 'email', 'unique:users']
         ]);
         // $request->validate([
         //     'email'=> ['required', 'email', 'unique:users']
@@ -25,13 +28,23 @@ class Registration extends Controller
         
         if($validator->fails()):
             return response()->json(['errors'=>$validator->errors()->all()]);
-        endif;
-        $email = $request->email;
-        if(Mail::to($email)->send(new StudentRegistration($email))):
-            return response()->json(['success'=>'success']);
         else:
-            return response()->json(['success'=>'success']);
+            $email = $request->email;
+            $token = Str::random(32);
+            $user = new User();
+            $user->email = $email;
+            $user->token = $token;
+
+            $user->save();
+            
+            if(Mail::to($email)->send(new StudentRegistration($email,$token))):
+                return response()->json(['success'=>'success']);
+            else:
+                return response()->json(['success'=>'success']);
+            endif;
         endif;
+
+
         
               
         // return view('website/registration', compact('msg'));
