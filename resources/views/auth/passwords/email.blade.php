d41921c191a352e06feefb7abfb1f210c303deb3
dinukolla <dinukolla@.github.com>
2020-12-18 14:12:33 +0530

    fogot password module

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,13 +1,35 @@
-@extends('layouts.app')
+@extends('layouts.web')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
 
-                <div class="card-body">
+
+<script type="text/javascript">
+
+    // ACTIVE NAVIGATION ENTRY
+    $(document).ready(function ($) {
+        $('#login_nav').addClass("menu-active");
+    });
+
+</script>
+
+    <!--==========================
+    Hero Section
+    ============================-->
+    <section id="hero" style="background-image: url({{ asset('img/background/back.jpg') }}); height:200px">
+        <div class="page-hero-container">
+            <h1 style="padding-top:30px">Login to FIT</h1>
+        </div>
+    </section><!-- #hero -->
+
+<main id="main">
+    <section id="registration" style="padding-top: 80px;">
+        <div class="row about-container mb-5 pb-5">
+            <div class="col-lg-6 background order-lg-1 order-2 pb-5"  style="background-image: url({{ asset('img/logo/login.png') }});">
+
+
+            </div>
+            <div class="col-lg-6 content order-lg-2 order-1">
+                <div class="p-5 form">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
@@ -18,10 +40,10 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-md-12 form-label">{{ __('E-Mail Address') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                            <div class="col-md-12">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}"  autocomplete="email" autofocus>
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -31,17 +53,19 @@
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                        <div class="form-group row mb-5">
+                            <div class="col-md-12 text-center">
+                                <button id="submit" type="submit" class="">
                                     {{ __('Send Password Reset Link') }}
                                 </button>
                             </div>
                         </div>
                     </form>
+                                                
                 </div>
             </div>
         </div>
-    </div>
-</div>
+    </section>
+</main>
+
 @endsection
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,22 +1,42 @@
-@extends('layouts.app')
+@extends('layouts.web')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
 
-                <div class="card-body">
+<script type="text/javascript">
+
+    // ACTIVE NAVIGATION ENTRY
+    $(document).ready(function ($) {
+        $('#login_nav').addClass("menu-active");
+    });
+
+</script>
+
+    <!--==========================
+    Hero Section
+    ============================-->
+    <section id="hero" style="background-image: url({{ asset('img/background/back.jpg') }}); height:200px">
+        <div class="page-hero-container">
+            <h1 style="padding-top:30px">Reset Password</h1>
+        </div>
+    </section><!-- #hero -->
+<main id="main">
+    <section id="registration" style="padding-top: 80px;">
+        <div class="row about-container mb-5 pb-5">
+            <div class="col-lg-7 background order-lg-1 order-2 pb-5"  style="background-image: url({{ asset('img/logo/login.png') }});">
+
+
+            </div>
+            <div class="col-lg-5 content order-lg-2 order-1">
+                <div class="p-5 form">
                     <form method="POST" action="{{ route('password.update') }}">
                         @csrf
 
                         <input type="hidden" name="token" value="{{ $token }}">
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-md-12 form-label">{{ __('E-Mail Address') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
@@ -28,10 +48,10 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            <label for="password" class="col-md-12 form-label">{{ __('Password') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                            <div class="col-md-12">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password"  autocomplete="new-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -42,24 +62,26 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                            <label for="password-confirm" class="col-md-12 form-label">{{ __('Confirm Password') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                            <div class="col-md-12">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation"  autocomplete="new-password">
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                        <div class="form-group row mb-5">
+                            <div class="col-md-12 text-center">
+                                <button id="submit" type="submit" class="">
                                     {{ __('Reset Password') }}
                                 </button>
                             </div>
                         </div>
                     </form>
+                                    
                 </div>
             </div>
         </div>
-    </div>
-</div>
+    </section>
+</main>
+
 @endsection
