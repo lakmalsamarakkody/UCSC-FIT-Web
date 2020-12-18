5f1cd52d9d9aa3726e8646291564b636281e7963
dinukolla <dinukolla@.github.com>
2020-12-18 16:45:31 +0530

    Update mail.php

--- config/mail.php
+++ config/mail.php
@@ -84,8 +84,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'no-reply@fit.bit.lk'),
+        'name' => env('MAIL_FROM_NAME', 'FIT - Portal'),
     ],
 
     /*
