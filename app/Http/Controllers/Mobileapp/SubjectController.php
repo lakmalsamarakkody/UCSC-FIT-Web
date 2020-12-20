92eba91c657ad63d7f120e4bda1cd4b123d1bc92
yasindudehan <ruwanwellayasindu@gmail.com>
2020-12-20 23:25:45 +0530

    subjectResultcontroller

--- app/Http/Controllers/Mobileapp/SubjectController.php
+++ app/Http/Controllers/Mobileapp/SubjectController.php
@@ -12,10 +12,12 @@ class SubjectController extends Controller
     return Subject::all();
    }
 
-   function subjectresult($id,$year=0){
-       if($year==0){
+   function subjectresult($id,$year=0,$month=''){
+       if($year==0 || $month==''){
 
            $year=idate('Y');
+           $month=date("F", mktime(0, 0, 0, 0, 10));
+
        }
    $data=DB::table('exam_schedules')
     ->join('subjects','exam_schedules.subject_id',"=",'subjects.id')
@@ -24,6 +26,7 @@ class SubjectController extends Controller
     ->join('exams','exam_schedules.exam_id',"=",'exams.id')
     ->where('subjects.id',$id)
     ->where('exams.year',$year)
+    ->where('exams.month',$month)
     ->select(
         'subjects.name',
         'subjects.code',
@@ -33,12 +36,78 @@ class SubjectController extends Controller
          'exams.year',
          'exams.month',
          'exam_schedules.id'
-    )
-    ->get();
-     $co=$data->count();
-     for($i=0; $i<$co; $i++){
+    );
+    $fullData=$data->get();
+     $co=$data  ->get()->count();
+     $pass=$data->where('student_exams.result','>',0)->orderBy('exams.month')->count();
+     $fail=$data->where('student_exams.result','<',1)->count();
+//    $pa=0;
+//    $average = array(
+//             array(
+//          'month' => 'January',
+//          'all'=>$data->where( "exams.month", "August",)->get(),
 
-     }
-    return $data->count();
+
+
+//             ),
+
+
+
+//         );
+    //  for($i=0; $i<$co; $i++){
+    //     $pass=$data->where('student_exams.result','1')->where( "exams.month", "October",)  ->get();
+    //    switch($fullData[$i]->month){
+    //            case "January":
+    //             $pa=$data->where('student_exams.result','1')->where( "exams.month", "January",)  ->count();
+    //            break;
+    //            case "February":
+    //             $pa=$data->where('student_exams.result','1')->where( "exams.month", "February",)  ->count();
+    //            break;
+    //            case 'March':
+    //             $pa=$data->where('student_exams.result','1')->where( "exams.month", "March",)  ->count();
+    //            break;
+    //            case 'April':
+    //             $pa=$data->where('student_exams.result','1')->where( "exams.month", "April",)  ->count();
+    //            break;
+    //            case 'May':
+    //             $pa=$data->where('student_exams.result','1')->where( "exams.month", "May",)  ->count();
+    //            break;
+    //            case 'June':
+    //             $pa=$data->where('student_exams.result','1')->where( "exams.month", "June",)  ->count();
+    //            break;
+    //            case 'July':
+    //             $pa=$data->where('student_exams.result','1')->where( "exams.month", "July",)  ->count();
+    //            break;
+    //            case 'August':
+    //             $pa=$data->where('student_exams.result','1')->where( "exams.month", "August",)  ->count();
+    //            break;
+    //            case 'September':
+    //             $pa=$data->where('student_exams.result','1')->where( "exams.month", "September",)  ->count();
+    //            break;
+    //            case 'October':
+    //             $pa=$data->where('student_exams.result','1')->where( "exams.month", "October",)  ->count();
+    //            break;
+    //            case 'November':
+    //             $pa=$data->where('student_exams.result','1')->where( "exams.month", "November",)  ->count();
+    //            break;
+    //         //    case 'December':
+    //         //     $pa=$data->where('student_exams.result','1')->where( "exams.month", "December",)  ->count();
+    //         //    break;
+    //    }
+    //  }
+    return
+
+        [
+
+            "Fail"=>$fail,
+            "Pass"=>$pass,
+            "Regstu"=>$co,
+
+        ];
+
+
+
+
+
 }
 }
