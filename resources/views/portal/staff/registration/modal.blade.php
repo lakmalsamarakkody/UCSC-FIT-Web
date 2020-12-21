58e1b79c7fe2cb6e2564a84786aafeea799a1798
Parakramaba <74170793+Parakramaba@users.noreply.github.com>
2020-12-21 15:30:55 +0530

    Applicant

--- resources/views/portal/staff/registration/modal.blade.php
+++ resources/views/portal/staff/registration/modal.blade.php
@@ -3,7 +3,7 @@
     <div class="modal-dialog modal-xl modal-dialog-scrollable modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="viewApplication">Apllicant Informtion</h5>
+                <h5 class="modal-title" id="viewApplication">Applicant Informtion</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
