352ffe49bc88698066a472f426d1e4a383a9e3ec
Parakramaba <74170793+Parakramaba@users.noreply.github.com>
2020-12-21 15:12:45 +0530

    Modal to view applicant details

---
+++ resources/views/portal/staff/registration/modal.blade.php
@@ -0,0 +1,169 @@
+<!-- VIEW APPLICANT DETAILS -->
+<div class="modal fade" id="modal-view-applicant" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="viewApplication" aria-hidden="true">
+    <div class="modal-dialog modal-xl modal-dialog-scrollable modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="viewApplication">Apllicant Informtion</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="col-lg-12 information">
+                    <div class="row">
+                        <div class="col-12">        
+                            <div class="card">
+                                <div class="card-header">
+                                    Personal Details
+                                </div>
+                                <div class="card-body">
+                                    <div class="row">
+                                        <div class="col-md-8 order-md-1 order-2">
+                                            <table class="table">
+                                                <tr>
+                                                    <th>Title:</th>
+                                                    <td>Mr.</td>
+                                                </tr>
+                                                <tr>
+                                                    <th>First Name:</th>
+                                                    <td>John</td>
+                                                </tr>
+                                                <tr>
+                                                    <th>Middle Names:</th>
+                                                    <td>John John John John John John John</td>
+                                                </tr>
+                                                <tr>
+                                                    <th>Initials:</th>
+                                                    <td>J J J J J J J J</td>
+                                                </tr>
+                                                <tr>
+                                                    <th>Last Name:</th>
+                                                    <td>Doe</td>
+                                                </tr>
+                                                <tr>
+                                                    <th>Full Name:</th>
+                                                    <td>John John John John John John John John Doe</td>
+                                                </tr>
+                                                <tr>
+                                                    <th>Gender:</th>
+                                                    <td><i class="fa fa-lg fa-male"></i></td>
+                                                </tr>
+                                                <tr>
+                                                    <th>Date of Birth:</th>
+                                                    <td>1990-04-01</td>
+                                                </tr>
+                                                <tr>
+                                                    <th>Citizenship:</th>
+                                                    <td>Sri Lankan</td>
+                                                </tr>
+                                                <tr>
+                                                    <th>NIC/ Postal/ Passport No:</th>
+                                                    <td>9012256545V</td>
+                                                </tr>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card mt-3">
+                                <div class="card-header">
+                                    Education Qualifications
+                                </div>
+                                <div class="card-body">                        
+                                    <div class="row">
+                                        <div class="col-12">
+                                            <table class="table">
+                                                <tr>
+                                                    <th>Highest Qualification</th>
+                                                    <td>Advanced/ Higher Diploma from a nationally or internationally recognized organisation</td>                                        
+                                                </tr>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card mt-3 contact">
+                                <div class="card-header ">
+                                    Contact Details
+                                </div>
+                                <div class="card-body">     
+                                    <div class="row">                                               
+                                        <div class="col-12 col-md-6">
+                                            <h5>Permanent Address</h5>
+                                            <hr>
+                                            <div class="ml-lg-4">
+                                               <p>House Name/ No.</p>  
+                                               <p>Adress Line 1 </p>
+                                               <p>Address Line 2</p>  
+                                               <p>Address Line 3</p>  
+                                               <p>Address Line 4 </p>
+                                               <p>City </p> 
+                                               <p>District/State</p>
+                                               <p>Country</p> 
+                                            </div>
+                                        </div>                    
+                                        <div class="col-12 col-md-6">
+                                            <h5>Current Address</h5>
+                                            <hr>
+                                            <div class="ml-lg-4">
+                                               <p>House Name/ No.</p>  
+                                               <p>Adress Line 1 </p>
+                                               <p>Address Line 2</p>  
+                                               <p>Address Line 3</p>  
+                                               <p>Address Line 4 </p>
+                                               <p>District/State</p>
+                                               <p>City </p> 
+                                               <p>Country</p> 
+                                            </div>
+                                        </div>
+                                        <div class="col-12 col-md-6">
+                                            <table class="table">
+                                                <tr>
+                                                    <th>Telephone No.</th>
+                                                    <td>+94 777 123 456</td>
+                                                </tr>
+                                            </table>
+                                        </div>
+                                        <div class="col-12 col-md-6">
+                                            <table class="table">
+                                                <tr>
+                                                    <th>Email</th>
+                                                    <td>johndoe@gmail.com</td>
+                                                </tr>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card mt-3">
+                                <div class="card-header">
+                                    Employment Details
+                                </div>
+                                <div class="card-body">                        
+                                    <div class="row">
+                                        <div class="col-12">
+                                            <table class="table">
+                                                <tr>
+                                                    <th>Designation</th>
+                                                    <td>CEO, Company Name, Address</td>                                        
+                                                </tr>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button class="btn btn-success">Approve</button>
+                <button class="btn btn-warning">Dismiss</button>
+            </div>
+        </div>
+    </div>
+
+</div>
+<!-- VIEW APPLICANT DETAILS -->
