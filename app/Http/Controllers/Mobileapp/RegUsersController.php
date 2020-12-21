fb2fd441f5573a8b044f31bea598689b15ffc53f
yasindudehan <ruwanwellayasindu@gmail.com>
2020-12-21 15:04:08 +0530

    change

--- app/Http/Controllers/Mobileapp/RegUsersController.php
+++ app/Http/Controllers/Mobileapp/RegUsersController.php
@@ -8,44 +8,27 @@ use App\Models\Student\Flag;
 use App\Models\Student_Registration\Academic_Year as Registed;
 use Hamcrest\Core\HasToString;
 use Symfony\Component\Finder\Exception\AccessDeniedException;
-
+use Illuminate\Support\Facades\DB;
 use function PHPSTORM_META\type;
 
 class RegUsersController extends Controller
 {
     function countRegUser(){
+        $data=DB::table('students')
+              ->where('students.reg_year',idate('Y'))
+              ->orderBy('updated_at', 'desc')
+              ->select(
+                  'students.reg_year',
+                  'students.first_name',
+                  'students.updated_at'
 
-      $co= Registed::count();
-      $regCount=0;
-    $data=Registed::all();
-    $date1=date_create(date("Y-m-d"));
-    $date2=date_create(date("Y-m-d"));
-    $diff=date_diff($date1,$date2);
+              );
 
-    for($i=0; $i<$co; $i++){
-
-        $date3=($data[$i]->start) ;
-
-          $date4=date_create($date3);
-
-        $diff=date_diff($date4,$date1);
-         $dayDiff=$diff->format("%R%a days");
-
-         if($dayDiff<366){
-                 $regCount++;
-         }
-
-
-
-
-        echo "\n";
-    }
-
-   // echo $diff->format("%R%a days");
 
        return [
-           "status"=>true,
-            "count"=>$regCount,
+            "yearReg"=>$data->count(),
+            'Data'=>$data->first(),
+
        ];
 
 
