d79651b102e475b1ffa2f6b6480dc3761fdbc315
Parakramaba <74170793+Parakramaba@users.noreply.github.com>
2020-12-22 11:10:30 +0530

    onclick functions

--- resources/views/portal/staff/registration/scripts.blade.php
+++ resources/views/portal/staff/registration/scripts.blade.php
@@ -1,4 +1,54 @@
 <script type="text/javascript">
 
+// DECLINE APPLICATION/PAYMENT
+decline_application_payment = () => {
+    SwalQuestionWarningAutoClose.fire({
+    title: "Are you sure?",
+    text: "You wont be able to revert this!",
+    confirmButtonText: 'Yes, Decline!',
+    })
+    .then((result) => {
+      if (result.isConfirmed) {
+        SwalDoneSuccess.fire({
+          title: 'Declined!',
+          text: 'Application/Payment declined. Message has been sent to the applicant.',
+        })
+        $('#modal-decline-message').modal('hide')
+        $('#modal-view-applicant').modal('hide')
+      }
+      else{
+        SwalNotificationWarningAutoClose.fire({
+          title: 'Cancelled!',
+          text: 'Application/Payment has not been declined.',
+        })
+      }
+    })
+  }
+  // /DECLINE APPLICATION/PAYMENT
+
+  // APPROVE APPLICATION
+  approve_application = () => {
+    SwalQuestionWarningAutoClose.fire({
+        title: "Are you sure?",
+        text: "You wont be able to revert this!",
+        confirmButtonText: 'Yes, Approve!',
+    })
+    .then((result) => {
+        if(result.isConfirmed) {
+            SwalDoneSuccess.fire({
+                title: 'Approved!',
+                text: 'The application has been approved.',
+            })
+            $('#modal-view-applicant').modal('hide')
+        }
+        else{
+            SwalNotificationWarningAutoClose.fire({
+                title: 'Cancelled!',
+                text: 'Application has not been approved.',
+            })
+        }
+    })
+  }
+  // /APPROVE APPLICATION
 
 </script>
