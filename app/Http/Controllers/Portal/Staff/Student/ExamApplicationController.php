559b39cbb6c6f7ce0c0a6f12cf50bd88dd5c2eee
Parakramaba <74170793+Parakramaba@users.noreply.github.com>
2020-12-23 11:14:40 +0530

    Add ExamApplicationController and Route

---
+++ app/Http/Controllers/Portal/Staff/Student/ExamApplicationController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Portal\Staff\Student;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ExamApplicationController extends Controller
+{
+    public function index()
+    {
+        return view('portal/staff/student/exams/exam_application');
+    }
+}
