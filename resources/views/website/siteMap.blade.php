cf96b1cbd32c82d10dec1a640e427df2c9cd0f87
dinukolla <dinukolla@.github.com>
2020-12-23 12:15:46 +0530

    Sitemap, policy and terms pages for website

---
+++ resources/views/website/siteMap.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts/web')
+@section('content')
+
+    <!--==========================
+    Hero Section
+    ============================-->
+    <section id="hero" style="background-image: url({{ asset('img/background/back.jpg') }}); height:200px">
+        <div class="page-hero-container">
+            <h1 style="padding-top:30px">Site Map</h1>
+        </div>
+    </section><!-- #hero -->
+
+    <main id="main">
+
+        <div class="col-12 p-5">
+ 
+        </div>
+
+    </main>
+@endsection
