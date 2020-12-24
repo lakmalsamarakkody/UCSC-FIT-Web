047e7149683e6db69aa2198ddfd7ffd0f78c71f3
dinukolla <dinukolla@.github.com>
2020-12-24 17:51:21 +0530

    Update siteMap.blade.php

--- resources/views/website/siteMap.blade.php
+++ resources/views/website/siteMap.blade.php
@@ -14,49 +14,50 @@
 
         <div class="col-12 p-5">
             <ul class="site-map-list">
-                <li><a href="">Home</a>
+                <li><a href="{{ url('/') }}">Home</a>
                     <ul>
-                        <li><a href="">Announcements</a></li>
-                       <li><a href="">About Us</a></li>
+                        <li><a href="{{ url('/') }}">Announcements</a></li>
+                       <li><a href="{{ url('/#about') }}">About Us</a></li>
                     </ul>
                 </li>
-                <li><a href="">Programe</a>
+                <li><a href="{{ url('/programme') }}">Programe</a>
                     <ul>
-                        <li><a href="">About FIT 103: ICT Applications</a></li>
-                        <li><a href="">About FIT 203: English for ICT</a></li>
-                        <li><a href="">About FIT 303: Mathematics for ICT</a></li>
+                        <li><a href="{{ url('/programme/#103') }}">About FIT 103: ICT Applications</a></li>
+                        <li><a href="{{ url('/programme/#203') }}">About FIT 203: English for ICT</a></li>
+                        <li><a href="{{ url('/programme/#303') }}">About FIT 303: Mathematics for ICT</a></li>
                     </ul>
                 </li>
-                <li><a href="">Learning</a>
+                <li><a href="{{ url('/learning') }}">Learning</a>
                     <ul>
-                        <li><a href="">Go to Virtual Learning Environment(VLE)</a></li>
+                        <li><a href="{{ url('/learning') }}">Go to Virtual Learning Environment(VLE)</a></li>
                     </ul>
                 </li>
-                <li><a href="">Examination</a>
+                <li><a href="{{ url('/examination') }}">Examination</a>
                     <ul>
-                        <li><a href="">Examination details</a></li>
-                        <li><a href="">Apply for Exams</a></li>
-                        <li><a href="">Contact UCSC e-Testing Lab</a></li>
+                        <li><a href="{{ url('/examination') }}">Examination details</a></li>
+                        <li><a href="{{ url('/examination') }}">Apply for Exams</a></li>
+                        <li><a href="{{ url('/examination/#contact') }}">Contact UCSC e-Testing Lab</a></li>
                     </ul>
                 </li>
-                <li><a href="">Registration</a>
+                <li><a href="{{ url('/registration') }}">Registration</a>
                     <ul>
-                        <li><a href="">Registration details</a></li>
-                        <li><a href="">Signup with Email</a></li>
-                        <li><a href="">If Registered, Login</a></li>
+                        <li><a href="{{ url('/registration') }}">Registration details</a></li>
+                        <li><a href="{{ url('/registration') }}">Signup with Email</a></li>
+                        <li><a href="{{ url('/registration') }}">If Registered, Login</a></li>
                     </ul>
                 </li>
-                <li><a href="">Contact Us</a>
+                <li><a href="{{ url('/contact') }}">Contact Us</a>
                     <ul>
-                        <li><a href="">Administration Matters</a></li>
-                        <li><a href="">e-Leaning Services</a></li>
-                        <li><a href="">Academic Matters</a></li>
-                        <li><a href="">Complaints</a></li>
-                        <li><a href="">Google Map Link to UCSC, Colombo, Sri Lanka</a></li>                        
+                        <li><a href="{{ url('/contact') }}">Administration Matters</a></li>
+                        <li><a href="{{ url('/contact') }}">e-Leaning Services</a></li>
+                        <li><a href="{{ url('/contact') }}">Academic Matters</a></li>
+                        <li><a href="{{ url('/contact') }}">Complaints</a></li>
+                        <li><a href="{{ url('/contact') }}">Google Map Link to UCSC, Colombo, Sri Lanka</a></li>                        
                     </ul>
                 </li>
-                <li><a href="">Frequently Asked Questions</a></li>
-                <li><a href="">Announcements</a></li>
+                <li><a href="{{ url('/downloads') }}">Downloads</a></li>
+                <li><a href="{{ url('/faq') }}">Frequently Asked Questions</a></li>
+                <li><a href="{{ url('/announcements') }}">Announcements</a></li>
             </ul>
         </div>
 
