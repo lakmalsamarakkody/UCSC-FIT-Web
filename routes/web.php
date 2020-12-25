2f6bf391b2197e9a93c9d4f5858ec692fcf0c232
Parakramaba <74170793+Parakramaba@users.noreply.github.com>
2020-12-25 18:53:40 +0530

    Add createSubject route

--- routes/web.php
+++ routes/web.php
@@ -85,6 +85,7 @@ Route::get('/portal/staff/system', [App\Http\Controllers\Portal\Staff\SystemCont
 Route::post('/portal/staff/system/createUserRole', [App\Http\Controllers\Portal\Staff\SystemController::class, 'createUserRole']);
 Route::post('/portal/staff/system/createStudentPhase', [App\Http\Controllers\Portal\Staff\SystemController::class, 'createStudentPhase']);
 Route::post('/portal/staff/system/createPermission', [App\Http\Controllers\Portal\Staff\SystemController::class, 'createPermission']);
+Route::post('/portal/staff/system/createSubject', [App\Http\Controllers\Portal\Staff\SystemController::class, 'createSubject']);
 // /STAFF SYSTEM PAGE CRUD OPERATIONS
 
 /*
