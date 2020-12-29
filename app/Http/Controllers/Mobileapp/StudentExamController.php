16563fff402a53b927f23da03889d78ecf885167
yasindudehan <ruwanwellayasindu@gmail.com>
2020-12-29 22:41:12 +0530

    change

--- app/Http/Controllers/Mobileapp/StudentExamController.php
+++ app/Http/Controllers/Mobileapp/StudentExamController.php
@@ -34,16 +34,16 @@ class StudentExamController extends Controller
        ->get()];
     }
     function currentExam(){
+        $ar=array();
+        $subjectcount=DB::table('subjects')->count();
+        for($i=1; $i<=$subjectcount; $i++){
         $scheId=DB::table('exam_schedules')
         ->join('student_exams','exam_schedules.id',"=",'student_exams.exam_schedule_id')
         ->join('subjects','exam_schedules.subject_id',"=",'subjects.id')
         ->orderBy('student_exams.updated_at', 'desc')
-        ->where('subjects.id',3)
+        ->where('subjects.id',$i)
         ->select('exam_schedules.id')
-        ->take(3)->get();
-           foreach($scheId as $sh){
-               echo $sh->id;
-           }
+        ->take(1)->get();
 
 
 
@@ -52,8 +52,8 @@ class StudentExamController extends Controller
         ->join('exam_types','exam_schedules.exam_type_id',"=",'exam_types.id')
         ->join('exams','exam_schedules.exam_id',"=",'exams.id')
         ->join('student_exams','exam_schedules.id',"=",'student_exams.exam_schedule_id')
-        ->where('student_exams.status','OK')
-        ->where('student_exams.exam_schedule_id',40)
+
+        ->where('student_exams.exam_schedule_id',$scheId[0]->id)
         ->select(
             'exam_schedules.date',
             'exam_schedules.start_time',
@@ -65,8 +65,24 @@ class StudentExamController extends Controller
             'exams.month',
             'exam_schedules.updated_at',
             'exam_schedules.id'
-            )
-        ->get();
-        return $data;
+        );
+        array_push
+        ($ar,
+           array(
+             "NotAB"=>$data->where('student_exams.status','OK')->count(),
+             "RegStu"=>$data->count(),
+             "details"=>$data->select
+             ('subjects.name',
+             'subjects.code',
+             'subjects.id',
+             'exams.year',
+             'exam_schedules.date',
+             'student_exams.updated_at')->first(),
+
+
+           )
+           );
+        }
+        return $ar;
     }
 }
