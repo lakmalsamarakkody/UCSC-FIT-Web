0e4b5f7d9c6af7280d840757592f7ed2c851c57d
lakmalsamarakkody <43323601+lakmalsamarakkody@users.noreply.github.com>
2021-01-05 23:04:39 +0530

    document table migration

---
+++ database/migrations/2021_01_05_145524_create_documents_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDocumentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('documents', function (Blueprint $table) {
+            $table->id();
+            $table->integer('student_id');
+            $table->string('type');
+            $table->string('side');
+            $table->string('image');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('documents');
+    }
+}
