884dca6fd687af6c5a47e2d9d3b0649482e25a32
dinukolla <dinukolla@.github.com>
2021-01-12 17:22:15 +0530

    sl cities sql and migration update

--- database/migrations/2020_12_15_101434_create_sl_cities_table.php
+++ database/migrations/2020_12_15_101434_create_sl_cities_table.php
@@ -13,21 +13,21 @@ class CreateSlCitiesTable extends Migration
      */
     public function up()
     {
-        Schema::create('sl_cities', function (Blueprint $table) {
-            $table->id();
-            $table->integer('district_id');
-            $table->string('name');
-            $table->string('name_si')->nullable();
-            $table->string('name_ta')->nullable();
-            $table->string('sub_name')->nullable();
-            $table->string('sub_name_si')->nullable();
-            $table->string('sub_name_ta')->nullable();             
-            $table->string('postcode')->nullable();
-            $table->double('latitude',20,10)->nullable();
-            $table->double('longitude',20,10)->nullable();
-            $table->timestamps();
-            $table->softDeletes();
-        });
+        // Schema::create('sl_cities', function (Blueprint $table) {
+        //     $table->id();
+        //     $table->integer('district_id');
+        //     $table->string('name');
+        //     $table->string('name_si')->nullable();
+        //     $table->string('name_ta')->nullable();
+        //     $table->string('sub_name')->nullable();
+        //     $table->string('sub_name_si')->nullable();
+        //     $table->string('sub_name_ta')->nullable();             
+        //     $table->string('postcode')->nullable();
+        //     $table->double('latitude',20,10)->nullable();
+        //     $table->double('longitude',20,10)->nullable();
+        //     $table->timestamps();
+        //     $table->softDeletes();
+        // });
     }
 
     /**
@@ -37,6 +37,6 @@ class CreateSlCitiesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('sl_cities');
+        // Schema::dropIfExists('sl_cities');
     }
 }
