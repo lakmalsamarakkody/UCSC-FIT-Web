18ec878dc3c6d8aa2ff72006e01df3157614de4d
Parakramaba <74170793+Parakramaba@users.noreply.github.com>
2021-01-16 01:26:19 +0530

    Months dropdown

--- resources/views/portal/staff/exams/exam_list.blade.php
+++ resources/views/portal/staff/exams/exam_list.blade.php
@@ -53,6 +53,16 @@
                       <option value="Default" selected disabled>Select Month</option>
                       <option value="January">January</option>
                       <option value="February">February</option>
+                      <option value="March">March</option>
+                      <option value="April">April</option>
+                      <option value="May">May</option>
+                      <option value="June">June</option>
+                      <option value="July">July</option>
+                      <option value="August">August</option>
+                      <option value="September">September</option>
+                      <option value="October">October</option>
+                      <option value="November">November</option>
+                      <option value="December">December</option>
                     </select>
                     <span class="invalid-feedback" id="error-examMonth" role="alert"></span>
                   </div>
