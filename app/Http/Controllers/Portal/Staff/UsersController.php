e67971f3b8deb8cefd2726dd53cd8116c4f9d685
dinukolla <dinukolla@.github.com>
2021-01-19 00:04:39 +0530

    Yajra Search complete

--- app/Http/Controllers/Portal/Staff/UsersController.php
+++ app/Http/Controllers/Portal/Staff/UsersController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Yajra\DataTables\Facades\DataTables;
 use App\Models\User;
 use App\Models\User\Role;
+use Illuminate\Support\Str;
 
 class UsersController extends Controller
 {
@@ -19,15 +20,34 @@ class UsersController extends Controller
 
   public function index()
   {
-    return view('portal/staff/users');
+    $roles = Role::all();
+
+    return view('portal/staff/users',compact('roles'));
   }
 
   public function getUserList(Request $request)
   {
     if ($request->ajax()) {
-      $data = User::addSelect(['role_name' => Role::select('name')->whereColumn('role_id', 'roles.id')])->get();
-      
+      $data = User::addSelect(['role_name' => Role::select('name')->whereColumn('role_id', 'roles.id')]);
+      if($request->name!=null){
+        $data = $data->where('name','like', '%'. $request->name.'%');
+      }
+      if($request->email!=null){
+        $data = $data->where('email','like', '%'. $request->email.'%');
+      }      
+      if($request->role!=null){
+        $data = $data->where('role_id', $request->role);
+      }
+      if($request->status!=null){
+        $data = $data->where('status', $request->status);
+      }
+      if($request->search!=null){
+        $data = $data->where('name','like', '%'. $request->search.'%')
+        ->orWhere('email','like', '%'. $request->search.'%');
+      }
+      $data = $data->get();
       return DataTables::of($data)
+
       ->addIndexColumn()
       ->addColumn('action', function($row){
           $actionBtn = '<a onclick="view_user();" title="View Profile" data-tooltip="tooltip"  data-placement="bottom"  type="button" class="btn btn-outline-primary"><i class="fas fa-user"></i></a>';
