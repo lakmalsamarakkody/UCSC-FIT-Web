c37f0188acec99fbd9512f109da3b10e1e7d9de2
lakmalsamarakkody <43323601+lakmalsamarakkody@users.noreply.github.com>
2021-01-18 12:39:29 +0530

    home page card count implemented

--- app/Http/Controllers/Portal/Staff/HomeController.php
+++ app/Http/Controllers/Portal/Staff/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Portal\Staff;
 use App\Http\Controllers\Controller;
 use App\Models\Exam;
 use App\Models\Exam\Schedule;
+use App\Models\Student\Registration;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -28,11 +29,26 @@ class HomeController extends Controller
      */
     public function index()
     {
-      $upcomings=Schedule::where('date', '>=', date('Y-m-d'))->orderby('date')->take(6)->get();
-      $dones=Schedule::where('date', '<', date('Y-m-d'))->orderby('date', 'desc')->take(6)->get();
-      return view('portal/staff/home',[
-          'upcomings' => $upcomings,
-          'dones' => $dones
-      ]);
+      // CARD COUNTS
+      $applicationCount = Registration::where('registered_at', NULL)->where('application_submit', '1')->where('application_status', NULL)->get()->count();
+      $paymentReviewCount = Registration::where('registered_at', NULL)->where('application_submit', '1')->where('payment_id', '!=', NULL)->where('payment_status', NULL)->get()->count();
+      $documentPendingCount = Registration::where('registered_at', NULL)->where('application_submit', '1')->where('payment_id', '!=', NULL)->where('payment_status', 'Approved')->where('document_submit', '0')->get()->count();
+      $documentReviewCount = Registration::where('registered_at', NULL)->where('application_submit', '1')->where('payment_id', '!=', NULL)->where('payment_status', 'Approved')->where('document_submit', '1')->where('document_status', NULL)->get()->count();
+      $pendingRegistration = Registration::where('registered_at', NULL)->where('application_submit', '1')->where('payment_id', '!=', NULL)->where('payment_status', 'Approved')->where('document_submit', '1')->where('document_status',  'Approved')->get()->count();
+      $totalRegistered = Registration::where('registered_at', '!=', NULL)->where('registration_expire_at', '>=', now())->where('application_submit', '1')->where('payment_id', '!=', NULL)->where('payment_status', 'Approved')->where('document_submit', '1')->where('document_status', 'Approved')->where('status', 1)->get()->count();
+      // /CARD COUNTS
+      
+      $upcomingExams=Schedule::where('date', '>=', date('Y-m-d'))->orderby('date')->take(6)->get();
+      $heldExams=Schedule::where('date', '<', date('Y-m-d'))->orderby('date', 'desc')->take(6)->get();
+      return view('portal/staff/home',compact(
+        'applicationCount',
+        'paymentReviewCount',
+        'documentPendingCount',
+        'documentReviewCount',
+        'pendingRegistration',
+        'totalRegistered',
+        'upcomingExams',
+        'heldExams'
+      ));
     }
 }
