691fe4b7b86a125ab524623eebfb6c9869f6a4ff
dinoaxK <dinushakulasooriya@gmail.com>
2021-02-10 14:58:36 +0530

    exams schedule buton dynamics handled

--- resources/views/portal/staff/exams/scripts.blade.php
+++ resources/views/portal/staff/exams/scripts.blade.php
@@ -57,11 +57,18 @@
         {
           targets: 7,
           render: function(data, type, row) {
-            var btnGroup = '<div class="btn-group">'+
-            '<button type="button" class="btn btn-outline-success" data-tooltip="tooltip" data-toggle="modal" data-placement="bottom" title="Approve" id="btnApproveSchedule-'+data+'" onclick="approve_schedule('+data+');"><i class="fas fa-file-signature"></i></button>'+
-            '<button type="button" class="btn btn-outline-info" data-tooltip="tooltip" data-placement="bottom" title="Request Approval" id="btnRequestApprovalSchedule-'+data+'" onclick="request_schedule_approval('+data+');"><i class="fas fa-share-square"></i></button>'+
-            '<button type="button" class="btn btn-outline-primary" data-tooltip="tooltip" data-toggle="modal" data-placement="bottom" title="Release" id="btnReleaseSchedule-'+data+'" onclick="relase_individual_schedule('+data+');" ><i class="fas fa-hand-point-right"></i></button>'+
-            '<button type="button" class="btn btn-outline-warning" data-tooltip="tooltip" data-placement="bottom" title="Edit" id="btnEditSchedule-'+data+'" onclick="edit_schedule_modal_invoke('+data+');"><i class="fas fa-edit"></i></button>'+
+            var btnGroup = '<div class="btn-group">';
+            if(row['schedule_approval']== null){
+                btnGroup = btnGroup + '<button type="button" class="btn btn-outline-info" data-tooltip="tooltip" data-placement="bottom" title="Request Approval" id="btnRequestApprovalSchedule-'+data+'" onclick="request_schedule_approval('+data+');"><i class="fas fa-share-square"></i></button>';
+            }
+            else if(row['schedule_approval'] == 'requested'){
+                btnGroup = btnGroup + '<button type="button" class="btn btn-outline-success" data-tooltip="tooltip" data-toggle="modal" data-placement="bottom" title="Approve" id="btnApproveSchedule-'+data+'" onclick="approve_schedule('+data+');"><i class="fas fa-file-signature"></i></button>'
+                + '<button type="button" class="btn btn-outline-danger" data-tooltip="tooltip" data-toggle="modal" data-placement="bottom" title="Decline" id="btnDeclineSchedule-'+data+'" onclick="decline_schedule('+data+');"><i class="fas fa-file-signature"></i></button>';
+            }
+            else if(row['schedule_approval']== 'approved'){
+                btnGroup = btnGroup + '<button type="button" class="btn btn-outline-primary" data-tooltip="tooltip" data-toggle="modal" data-placement="bottom" title="Release" id="btnReleaseSchedule-'+data+'" onclick="relase_individual_schedule('+data+');" ><i class="fas fa-hand-point-right"></i></button>';
+            }
+            btnGroup = btnGroup + '<button type="button" class="btn btn-outline-warning" data-tooltip="tooltip" data-placement="bottom" title="Edit" id="btnEditSchedule-'+data+'" onclick="edit_schedule_modal_invoke('+data+');"><i class="fas fa-edit"></i></button>'+
             '<button type="button" class="btn btn-outline-danger" data-tooltip="tooltip" data-placement="bottom" title="Delete" id="btnDeleteExamSchedule-'+data+'" onclick="delete_before_release('+data+');"><i class="fas fa-trash-alt"></i></button>'+
             '</div>';
             return btnGroup;
@@ -130,14 +137,14 @@
             '<button type="button" class="btn btn-outline-danger" data-tooltip="tooltip" data-placement="bottom" title="Delete" id="btnDeleteAfterRelease-'+data+'" onclick="delete_after_release('+data+');"><i class="fas fa-trash-alt"></i></button>'+
             '</div>';
             return btnGroup;
-            
+
           }
         }
       ]
     });
     // /After Release table
     // /TABLES
-    
+
     // UPCOMING EXAMS(before release)
     // Create schedule
     create_schedule = () => {
