2557b4d526c4e727ebc7e833e7db67d4e52c2a93
dinoaxK <dinushakulasooriya@gmail.com>
2021-02-17 17:31:05 +0530

    migration, model and seeder for exam duration table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -42,6 +42,7 @@ class DatabaseSeeder extends Seeder
             WorldDivisionsTableSeeder::class,
             WorldCitiesTableSeeder::class,
             FeeSeeder::class,
+            DurationSeeder::class,
         ]);
     }
 }
