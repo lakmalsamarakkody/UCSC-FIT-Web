1be2ea8faf668fd88c86f5cea6f90ce098dc39fc
yasindudehan <ruwanwellayasindu@gmail.com>
2021-02-22 15:29:28 +0530

    change

--- app/Http/Controllers/Mobileapp/SubjectController.php
+++ app/Http/Controllers/Mobileapp/SubjectController.php
@@ -13,7 +13,7 @@ class SubjectController extends Controller
     return Subject::all();
    }
 
-   function subjectresult($id,$year=0,$month=''){
+   function subjectresult($id,$sheId,$year=0,$month=''){
     $now = Carbon::now();
     $ar=array();
        if($year==0){
@@ -26,37 +26,38 @@ class SubjectController extends Controller
         $month=$now->month;
        
        }
-   $sheduleIds=DB::table('exam_schedules')
-    ->join('subjects','exam_schedules.subject_id',"=",'subjects.id')
-    //->join('student_exams','exam_schedules.id',"=",'student_exams.exam_schedule_id')
-    // ->join('students','student_exams.student_id',"=",'students.id')
-     ->join('exams','exam_schedules.exam_id',"=",'exams.id')
-     ->join('exam_types',"exam_schedules.exam_type_id","=",'exam_types.id')
-    ->where('subjects.id',$id)
-    ->where('exams.year',$year)
-     ->where('exams.month',$month)
-    ->select(
-        'subjects.name',
-        'subjects.code',
-        // 'student_exams.result',
-        // 'student_exams.mark',
-        'exam_schedules.date',  
-         'exams.year',
-         'exams.month',
-         'exam_schedules.id',
-         'exam_types.name'
-    )->get();
-   $idcount=$sheduleIds->count();
-   for ($i=0; $i<$idcount; $i++) { 
+//    $sheduleIds=DB::table('exam_schedules')
+//     ->join('subjects','exam_schedules.subject_id',"=",'subjects.id')
+//     //->join('student_exams','exam_schedules.id',"=",'student_exams.exam_schedule_id')
+//     // ->join('students','student_exams.student_id',"=",'students.id')
+//      ->join('exams','exam_schedules.exam_id',"=",'exams.id')
+//      ->join('exam_types',"exam_schedules.exam_type_id","=",'exam_types.id')
+//     ->where('subjects.id',$id)
+//     ->where('exams.year',$year)
+//      ->where('exams.month',$month)
+//     ->select(
+//         'subjects.name',
+//         'subjects.code',
+//         // 'student_exams.result',
+//         // 'student_exams.mark',
+//         'exam_schedules.date',  
+//          'exams.year',
+//          'exams.month',
+//          'exam_schedules.id',
+//          'exam_types.name'
+//     )->get();
+//    $idcount=$sheduleIds->count();
+//    for ($i=0; $i<$idcount; $i++) { 
     $data=DB::table('exam_schedules')
     ->join('subjects','exam_schedules.subject_id',"=",'subjects.id')
     ->join('student_exams','exam_schedules.id',"=",'student_exams.exam_schedule_id')
      ->join('students','student_exams.student_id',"=",'students.id')
      ->join('exams','exam_schedules.exam_id',"=",'exams.id')
+     ->join('exam_types','exam_schedules.exam_type_id',"=",'exam_types.id')
     ->where('subjects.id',$id)
     ->where('exams.year',$year)
      ->where('exams.month',$month)
-     ->where('student_exams.exam_schedule_id',$sheduleIds[$i]->id)
+     ->where('student_exams.exam_schedule_id',$sheId)
      ->select(
         'subjects.name',
         'subjects.code',
@@ -65,15 +66,16 @@ class SubjectController extends Controller
         'exam_schedules.date',  
          'exams.year',
          'exams.month',
-         'exam_schedules.id'
+         'exam_schedules.id',
+         'exam_types.name'
     );
     // ->whrere('student_exams.exam_schedule_id',$sheduleIds[$i]->id)
     array_push
     ($ar,
        array(
     
-        "Shedule ID"=>$sheduleIds[$i]->id,
-        "typeName"=>$sheduleIds[$i]->name,
+        "Shedule ID"=>$data->first()->id,
+        "typeName"=>$data->first()->name,
          "NotAB"=>$data->where('student_exams.status','OK')->count(),
          "RegStu"=>$data->count(),
 
@@ -83,15 +85,14 @@ class SubjectController extends Controller
 
        )
        );
-   }
+//    }
    
     // $absent=$data->where('student_exams.status','OK')->count();
     //  $co=$data  ->count();
     //  $pass=$data->where('student_exams.result','>',0)->orderBy('exams.month')->count();
     //  $fail=$data->where('student_exams.result','<',1)->count();
 
-    return
-            $ar;
+    return $ar;
      
 
 
