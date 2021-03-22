3102328ae2360657ef353b7d00bbd073dca21ad2
Parakramaba <74170793+Parakramaba@users.noreply.github.com>
2021-03-22 12:17:52 +0530

    Update student exams table

--- database/migrations/2021_02_19_091452_add_columns_to_student_exams_table.php
+++ database/migrations/2021_02_19_091452_add_columns_to_student_exams_table.php
@@ -19,9 +19,6 @@ class AddColumnsToStudentExamsTable extends Migration
             $table->integer('requested_exam_id')->after('exam_type_id');
             $table->string('payment_status')->nullable()->after('payment_id');
             $table->text('declined_message')->nullable()->after('payment_status');
-            $table->string('medical_reason')->nullable()->after('declined_message');
-            $table->string('medical_image')->nullable()->after('medical_reason');
-            $table->string('medical_status')->nullable()->after('medical_image');
         });
     }
 
