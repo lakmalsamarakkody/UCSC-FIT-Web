832865642cd14bc3c4d09b6de2ccbb7313e079cb
Parakramaba <74170793+Parakramaba@users.noreply.github.com>
2021-03-30 16:10:40 +0530

    Student profile open in new tab

--- resources/views/portal/staff/student/scripts.blade.php
+++ resources/views/portal/staff/student/scripts.blade.php
@@ -104,9 +104,9 @@
             var url = '{{ route("student.profile", ":id") }}';
             url = url.replace(':id', id);
                     //    alert (id)
-                    let params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,
-width=1500,height=700,left=100,top=100`;
-            window.open( url,'Student_Profile',params)
+//                     let params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,
+// width=1500,height=700,left=100,top=100`;
+            window.open( url,'Student_Profile')
         }
         
         $(".collapse.show").each(function(){
