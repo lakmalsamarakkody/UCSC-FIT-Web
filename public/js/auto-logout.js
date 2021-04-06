7d0bf1067e907ec47fde4103a1497e60f168b4b4
lakmalsamarakkody <43323601+lakmalsamarakkody@users.noreply.github.com>
2021-04-06 17:21:51 +0530

    auto logout javascript function implemented

---
+++ public/js/auto-logout.js
@@ -0,0 +1,35 @@
+//initialize
+var timoutNow = 5000;
+var timeoutTimerID;
+
+//start timer
+function startTimer() {
+  // window.setTimeout returns an Id that can be used to start and stop a timer
+  timeoutTimerID = window.setTimeout(IdleTimeout, timoutNow);
+}
+
+//reset timer
+function resetTimer() {
+  window.clearTimeout(timeoutTimerID);
+  startTimer();
+}
+
+// Logout the user.
+function IdleTimeout() {
+  document.getElementById('logout-form').submit();
+}
+
+//timer setup
+function setupTimers () {
+  document.addEventListener("mousemove", resetTimer, false);
+  document.addEventListener("mousedown", resetTimer, false);
+  document.addEventListener("keypress", resetTimer, false);
+  document.addEventListener("touchmove", resetTimer, false);
+  document.addEventListener("onscroll", resetTimer, false);
+  startTimer();
+}
+
+//run timer automatically
+$(document).ready(function(){
+  setupTimers();
+});
