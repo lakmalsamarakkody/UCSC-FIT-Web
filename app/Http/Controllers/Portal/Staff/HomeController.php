c7066c13d3df31547f236110dcb38a66e285dfac
lakmalsamarakkody <43323601+lakmalsamarakkody@users.noreply.github.com>
2021-04-09 18:20:05 +0530

    Update HomeController.php

--- app/Http/Controllers/Portal/Staff/HomeController.php
+++ app/Http/Controllers/Portal/Staff/HomeController.php
@@ -33,12 +33,12 @@ class HomeController extends Controller
     {
       // CARD COUNTS
       // Registration
-      $applicationCount = Registration::where('registered_at', NULL)->where('application_submit', '1')->where('application_status', NULL)->where('payment_id', NULL)->get()->count();
-      $paymentReviewCount = Registration::where('registered_at', NULL)->where('application_submit', '1')->where('payment_id', '!=', NULL)->where('payment_status', NULL)->get()->count();
-      $documentPendingCount = Registration::where('registered_at', NULL)->where('application_submit', '1')->where('payment_id', '!=', NULL)->where('payment_status', 'Approved')->where('document_submit', '0')->get()->count();
-      $documentReviewCount = Registration::where('registered_at', NULL)->where('application_submit', '1')->where('payment_id', '!=', NULL)->where('payment_status', 'Approved')->where('document_submit', '1')->where('document_status', NULL)->get()->count();
-      $pendingRegistration = Registration::where('registered_at', NULL)->where('application_submit', '1')->where('payment_id', '!=', NULL)->where('payment_status', 'Approved')->where('document_submit', '1')->where('document_status',  'Approved')->get()->count();
-      $totalRegistered = Registration::where('registered_at', '!=', NULL)->where('registration_expire_at', '>=', now())->where('application_submit', '1')->where('payment_id', '!=', NULL)->where('payment_status', 'Approved')->where('document_submit', '1')->where('document_status', 'Approved')->where('status', 1)->get()->count();
+      $applicationCount = Registration::where('registered_at', NULL)->where('application_submit', '1')->where('application_status', NULL)->get()->count();
+      $paymentReviewCount = Registration::where('registered_at', NULL)->where('application_submit', '1')->where('application_status', "Approved")->where('payment_id', '!=', NULL)->where('payment_status', NULL)->get()->count();
+      $documentPendingCount = Registration::where('registered_at', NULL)->where('application_submit', '1')->where('application_status', "Approved")->where('payment_id', '!=', NULL)->where('payment_status', 'Approved')->where('document_submit', '0')->get()->count();
+      $documentReviewCount = Registration::where('registered_at', NULL)->where('application_submit', '1')->where('application_status', "Approved")->where('payment_id', '!=', NULL)->where('payment_status', 'Approved')->where('document_submit', '1')->where('document_status', NULL)->get()->count();
+      $pendingRegistration = Registration::where('registered_at', NULL)->where('application_submit', '1')->where('application_status', "Approved")->where('payment_id', '!=', NULL)->where('payment_status', 'Approved')->where('document_submit', '1')->where('document_status',  'Approved')->get()->count();
+      $totalRegistered = Registration::where('registered_at', '!=', NULL)->where('registration_expire_at', '>=', now())->where('application_submit', '1')->where('application_status', "Approved")->where('payment_id', '!=', NULL)->where('payment_status', 'Approved')->where('document_submit', '1')->where('document_status', 'Approved')->where('status', 1)->get()->groupBy('student_id')->count();
 
       //Exams
       $examPaymentReviewCount = hasExam::where('payment_id', '!=', null)->where('payment_status', null)->where('schedule_status', 'Pending')->get()->unique('payment_id')->count();
