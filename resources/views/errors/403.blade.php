aa77ce356d412e79f8d08234eae2efcc9debb2b0
lakmalsamarakkody <43323601+lakmalsamarakkody@users.noreply.github.com>
2021-04-09 18:19:54 +0530

    Update 403.blade.php

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -1,5 +1,5 @@
 @extends('errors::illustrated-layout')
 
-@section('title', __('Forbidden'))
+@section('title', __('Link Expired'))
 @section('code', '403')
-@section('message', __($exception->getMessage() ?: 'Forbidden'))
+@section('message', __($exception->getMessage() ?: 'Link Expired'))
