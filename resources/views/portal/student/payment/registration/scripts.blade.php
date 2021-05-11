01243cc049c8a8be5207475cb9af37858f13302e
lakmalsamarakkody <43323601+lakmalsamarakkody@users.noreply.github.com>
2021-05-11 16:43:00 +0530

    re-registration payment submit script implemented

--- resources/views/portal/student/payment/registration/scripts.blade.php
+++ resources/views/portal/student/payment/registration/scripts.blade.php
@@ -3,7 +3,6 @@
   // SAVE PAYMENT
   save_payment = () => {
 
-    
     // FORM PAYLOAD
     var formData = new FormData($("#registerPaymentForm")[0]);
     $('.form-control').removeClass('is-invalid');
@@ -13,7 +12,7 @@
       headers: {
         'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
       },
-      url: "{{ url('/portal/student/payment/registration') }}",
+      url: "{{ route('payment.registration.submit') }}",
       type: 'post',
       data: formData,
       processData: false,
@@ -45,7 +44,7 @@
           });
         }else if (data['error']){
           SwalSystemErrorDanger.fire({
-            title: 'PaymentFailed!',
+            title: 'Payment Failed!',
             text: 'Please Try Again or Contact Administrator: admin@fit.bit.lk',
           })
         }
@@ -54,12 +53,73 @@
         $("#spinner").addClass('d-none');
         $('#btnSavePayment').removeAttr('disabled');
         SwalSystemErrorDanger.fire({
-          title: 'PaymentFailed!',
+          title: 'Payment Failed!',
           text: 'Please Try Again or Contact Administrator: admin@fit.bit.lk',
         })
       }
     });
   }
   // /SAVE PAYMENT
+
+  // SAVE REREGISTRATION PAYMENT
+  save_reregistration_payment = () => {
+
+    // FORM PAYLOAD
+    var formData = new FormData($("#registerPaymentForm")[0]);
+    $('.form-control').removeClass('is-invalid');
+    $('.invalid-feedback').html('');
+    $('.invalid-feedback').hide();
+    $.ajax({
+      headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+      },
+      url: "{{ route('payment.reregistration.submit') }}",
+      type: 'post',
+      data: formData,
+      processData: false,
+      contentType: false,           
+      beforeSend: function(){
+        // Show loader
+        $("#spinner").removeClass('d-none');
+        $('#btnSavePayment').attr('disabled','disabled');
+      },
+      success: function(data){
+        $("#spinner").addClass('d-none');
+        $('#btnSavePayment').removeAttr('disabled');
+        if(data['errors']){
+          $.each(data['errors'], function(key, value){
+            $('#error-'+key).show();
+            $('#'+key).addClass('is-invalid');
+            $('#error-'+key).append('<strong>'+value+'</strong>');
+            window.location.hash = '#'+key;
+          });
+        }else if (data['success']){
+          $('.form-control').val('');
+          SwalDoneSuccess.fire({
+            title: 'Payment Submitted to review!',
+            text: 'You\'ll be notified once reviewed',
+          }).then((result) => {
+            if(result.isConfirmed) {
+              window.location.href = '/portal/student/';
+            }
+          });
+        }else if (data['error']){
+          SwalSystemErrorDanger.fire({
+            title: 'Payment Failed!',
+            text: 'Please Try Again or Contact Administrator: admin@fit.bit.lk',
+          })
+        }
+      },
+      error: function(err){
+        $("#spinner").addClass('d-none');
+        $('#btnSavePayment').removeAttr('disabled');
+        SwalSystemErrorDanger.fire({
+          title: 'Payment Failed!',
+          text: 'Please Try Again or Contact Administrator: admin@fit.bit.lk',
+        })
+      }
+    });
+  }
+  // /SAVE REREGISTRATION PAYMENT
 </script>
 @endsection
