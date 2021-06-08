9737b242fd02ac8c97969d1475cd790e71d55c5f
dinukolla <dinukolla@.github.com>
2021-06-08 16:35:19 +0530

    result view updated

--- resources/views/portal/staff/result/view/scripts.blade.php
+++ resources/views/portal/staff/result/view/scripts.blade.php
@@ -6,5 +6,11 @@
      });
 
   });
+
+    view_student = (id) => {
+      var url = '{{ route("student.profile", ":id") }}';
+      url = url.replace(':id', id);
+      window.open( url,'Student_Profile')
+    }
 </script>
 @endsection
