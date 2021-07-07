171e0bf0abc9bc2265d7d943662213922f5ce25a
dinukolla <59581674+dinoaxK@users.noreply.github.com>
2021-07-07 16:48:20 +0530

    migrations for jobs table

---
+++ database/migrations/2021_07_07_142507_add_columns_to_exam_schedules_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnsToExamSchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('exam_schedules', function (Blueprint $table) {
+            $table->integer('lab')->after('exam_type_id');
+            $table->integer('lab_capacity')->after('exam_type_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('exam_schedules', function (Blueprint $table) {
+            //
+        });
+    }
+}
