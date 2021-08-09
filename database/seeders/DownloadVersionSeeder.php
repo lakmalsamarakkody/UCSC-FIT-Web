351658e459ccc0d011e15a3aee7d7db3bdcfd01a
Dinusha Kulasooriya <dinushakulasooriya@gmail.com>
2021-08-09 21:32:21 +0530

    Update DownloadVersionSeeder.php

--- database/seeders/DownloadVersionSeeder.php
+++ database/seeders/DownloadVersionSeeder.php
@@ -13,17 +13,15 @@ class DownloadVersionSeeder extends Seeder
      * @return void
      */
     public function run()
-    {
-        for ($i=1; $i < 717; $i++) { 
-            DownloadVersion::insert(                
-                array(
-                    [
-                        'id'=>$i,
-                        'created_at'=> date('Y-m-d H:i:s'),
-                        'updated_at'=> date('Y-m-d H:i:s')
-                    ]
-                )
-            );
-        }
+    { 
+        DownloadVersion::insert(                
+            array(
+                [
+                    'id'=>716,
+                    'created_at'=> date('Y-m-d H:i:s'),
+                    'updated_at'=> date('Y-m-d H:i:s')
+                ]
+            )
+        );
     }
 }
